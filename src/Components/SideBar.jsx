537733c550648b66ffd86b4b335293a62d173b96
Ajay Umakanth <ajay@heidi-app.de>
2023-01-10 13:15:10 +0100

    Initial responsive navbar

---
+++ src/Components/SideBar.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import logo from '../Resource/HEIDI_Logo.png';
+
+function SideBar () {
+  function openSidebar() {
+    let sideBar = document.querySelector(".sidebar")
+    if (sideBar)
+    {
+      sideBar.classList.toggle("hidden");
+    }
+  }
+  return (
+    <div>
+      <span
+        className="absolute text-white text-4xl top-5 left-4 bg-black cursor-pointer"
+        onClick={openSidebar}
+      >
+        <svg className="h-8 p-2 fill-current cursor-pointer" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+          <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/>
+        </svg>
+      </span>
+      <div className="sidebar fixed top-0 bottom-0 lg:left-0 p-2 w-[300px] overflow-y-auto text-center bg-black">
+        <div className="text-gray-100 text-xl">
+          <div className="p-2.5 mt-1 flex items-center">
+            <h1 className="font-bold text-gray-200 text-[15px] ml-3">HEIDI</h1>
+            <svg className="h-6 fill-current cursor-pointer ml-28 lg:hidden" onClick={openSidebar} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
+              <path d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z"/>
+            </svg>
+          </div>
+          <div className="my-2 bg-gray-600 h-[1px]"></div>
+        </div>
+        <div
+          className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-slate-600 text-white"
+        >
+          <svg className="h-6 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
+            <path d="M224 256c70.7 0 128-57.3 128-128S294.7 0 224 0 96 57.3 96 128s57.3 128 128 128zm89.6 32h-16.7c-22.2 10.2-46.9 16-72.9 16s-50.6-5.8-72.9-16h-16.7C60.2 288 0 348.2 0 422.4V464c0 26.5 21.5 48 48 48h274.9c-2.4-6.8-3.4-14-2.6-21.3l6.8-60.9 1.2-11.1 7.9-7.9 77.3-77.3c-24.5-27.7-60-45.5-99.9-45.5zm45.3 145.3l-6.8 61c-1.1 10.2 7.5 18.8 17.6 17.6l60.9-6.8 137.9-137.9-71.7-71.7-137.9 137.8zM633 268.9L595.1 231c-9.3-9.3-24.5-9.3-33.8 0l-37.8 37.8-4.1 4.1 71.8 71.7 41.8-41.8c9.3-9.4 9.3-24.5 0-33.9z"/>
+          </svg>
+          <span className="text-[15px] ml-4 text-gray-200 font-bold">My Entries</span>
+        </div>
+        <div
+          className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-slate-600 text-white"
+        >
+          <svg className="h-6 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
+            <path d="M248 8C111 8 0 119 0 256s111 248 248 248 248-111 248-248S385 8 248 8zm0 96c48.6 0 88 39.4 88 88s-39.4 88-88 88-88-39.4-88-88 39.4-88 88-88zm0 344c-58.7 0-111.3-26.6-146.5-68.2 18.8-35.4 55.6-59.8 98.5-59.8 2.4 0 4.8.4 7.1 1.1 13 4.2 26.6 6.9 40.9 6.9 14.3 0 28-2.7 40.9-6.9 2.3-.7 4.7-1.1 7.1-1.1 42.9 0 79.7 24.4 98.5 59.8C359.3 421.4 306.7 448 248 448z"/>
+          </svg>
+          <span className="text-[15px] ml-4 text-gray-200 font-bold">My Profile</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SideBar;
