936ba44675b660d6fa7edd579a898c3f5e49b5f5
Ajay Umakanth <ajay@heidi-app.de>
2023-01-17 14:22:36 +0100

    Add a responsive frontend page for profile page

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -19,17 +19,6 @@ function SideBar () {
     }
   }
 
-  // function windowResize() {
-  //   let sideBar = document.querySelector(".sidebar")
-  //   if (window.innerWidth <= 768) 
-  //     sideBar.classList.add('hidden');
-  //   else 
-  //     sideBar.classList.remove('hidden');
-  // }
-  
-  // window.addEventListener("resize", windowResize);
-  // window.addEventListener("load", windowResize);
-
   return (
     <div>
       <span
@@ -40,7 +29,7 @@ function SideBar () {
           <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/>
         </svg>
       </span>
-      <div className="sidebar fixed top-0 bottom-0 lg:left-0 p-2 w-[300px] z-50 overflow-y-auto text-center bg-black">
+      <div className="sidebar fixed top-0 bottom-0 p-2 w-[300px] z-50 overflow-y-auto text-center bg-black">
         <div className="text-gray-100 text-xl">
           <div className="p-2.5 mt-1 flex items-center">
             <img onClick={() => navigateTo('/Dashboard')} className="p-5 cursor-pointer" src={logo}/>
@@ -48,7 +37,7 @@ function SideBar () {
               className="text-white bg-black cursor-pointer"
               onClick={openSidebar}
             >
-              <svg className="fixed top-0 left-[270px] h-8 p-2 z-5 fill-current cursor-pointer md:hidden" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
+              <svg className="fixed top-0 left-[270px] h-8 p-2 z-5 fill-current cursor-pointer close-nav-bar" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
                 <path d="M310.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L160 210.7 54.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L114.7 256 9.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 301.3 265.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L205.3 256 310.6 150.6z"/>
               </svg>
             </span>
--- src/Path/ProfilePage.jsx
+++ src/Path/ProfilePage.jsx
@@ -1,14 +1,221 @@
 import React from 'react';
 import SideBar from '../Components/SideBar';
+import './bodyContainer.css';
+import { useNavigate } from "react-router-dom";
   
 const ProfilePage =() =>{
+
+    let navigate = useNavigate();
+    const navigateTo = (path) => {
+      if (path) {
+        navigate(path);
+      }
+    }
+
     return (
     <div>
         <SideBar/>
-    <div>
-        <h1>Profile Page</h1>
-        <p>This is the Profile Page</p>
-    </div>
+        <div class="container w-auto px-5 py-2 bg-slate-600">
+            <div class="bg-white mt-4 p-6">
+                <h2 class="text-gray-900 text-lg mb-4 font-medium title-font">
+                    Account
+                    <div className="my-4 bg-gray-600 h-[1px]"/>
+                </h2>
+                <div class="relative mb-4">
+                    <div class="pb-8">
+                        <label
+                        for="category"
+                        class="block px-2 text-lg font-medium text-gray-600"
+                        >
+                        Profile
+                        </label>
+                        <label
+                        for="category"
+                        class="block px-2 text-sm font-medium text-gray-400"
+                        >
+                        This information will be displayed publicly, so be careful what you share
+                        </label>
+                    </div>
+                    <div class="py-2 grid grid-cols-1 md:grid-cols-2">
+                        <div class="mt-1 px-2">
+                            <label
+                            for="category"
+                            class="block text-md font-medium text-gray-600"
+                            >
+                                First Name
+                            </label>
+                            <input
+                                type="text"
+                                name="category"
+                                id="category"
+                                class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                                placeholder="Max"
+                            />
+                        </div>
+                        <div class="mt-1 px-2">
+                            <label
+                            for="category"
+                            class="block text-md font-medium text-gray-600"
+                            >
+                                Last Name
+                            </label>
+                            <input
+                                type="text"
+                                name="category"
+                                id="category"
+                                class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                                placeholder="Mullerman"
+                            />
+                        </div>
+                    </div>
+                    <div class="py-3 grid grid-cols-1">
+                        <div class="mt-1 px-2">
+                        <label
+                        for="category"
+                        class="block text-md font-medium text-gray-600"
+                        >
+                            Username
+                        </label>
+                        <input
+                            type="text"
+                            name="category"
+                            id="category"
+                            class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                            placeholder="Max123"
+                        />
+                        </div>
+                    </div>
+                    <div class="py-3 grid grid-cols-1">
+                        <div class="mt-1 px-2">
+                        <label
+                        for="category"
+                        class="block text-md font-medium text-gray-600"
+                        >
+                            Photo
+                        </label>
+                        <div class="py-2 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-8">
+                            <div class="flex flex-col justify-center items-center">
+                                <img class="rounded-full h-20" src="https://i.ibb.co/L1LQtBm/Ellipse-1.png" alt="avatar" />
+                            </div>
+                            <div class="flex flex-col justify-center items-center">
+                                <button class="bg-black hover:bg-slate-600 text-white font-bold py-2 px-4 rounded">
+                                    Change
+                                </button>
+                            </div>
+                            <div class="flex flex-col justify-center items-center">
+                                <button class="bg-black hover:bg-slate-600 text-white font-bold py-2 px-4 rounded content-center">
+                                    Remove
+                                </button>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    <div class="py-3 grid grid-cols-1">
+                        <div class="mt-1 px-2">
+                        <label
+                        for="category"
+                        class="block text-md font-medium text-gray-600"
+                        >
+                            Description
+                        </label>
+                        <textarea
+                            type="text"
+                            name="category"
+                            id="category"
+                            class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                            placeholder="Write a description about yourself"
+                        />
+                        </div>
+                    </div> 
+                    <div class="py-3 grid grid-cols-1">
+                        <div class="mt-1 px-2">
+                        <label
+                        for="category"
+                        class="block text-md font-medium text-gray-600"
+                        >
+                            URL/Website
+                        </label>
+                        <input
+                            type="text"
+                            name="category"
+                            id="category"
+                            class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                            placeholder="www.example.com"
+                        />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container w-auto px-5 py-2 bg-slate-600">
+            <div class="bg-white mt-4 p-6">
+                <h2 class="text-gray-900 text-lg mb-4 font-medium title-font">
+                    Personal Information
+                    <div className="my-4 bg-gray-600 h-[1px]"/>
+                </h2>
+                <div class="relative mb-4">
+                    <div class="pb-8">
+                        <label
+                        for="category"
+                        class="block px-2 text-sm font-medium text-gray-400"
+                        >
+                        This information will be displayed publicly, so be careful what you share
+                        </label>
+                    </div>
+                    <div class="py-2 grid grid-cols-1 md:grid-cols-2">
+                        <div class="mt-1 px-2">
+                        <label
+                        for="category"
+                        class="block text-md font-medium text-gray-600"
+                        >
+                            Email
+                        </label>
+                        <input
+                            type="text"
+                            name="category"
+                            id="category"
+                            class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                            placeholder="abc@def.com"
+                        />
+                        </div>
+                        <div class="mt-1 px-2">
+                        <label
+                        for="category"
+                        class="block text-md font-medium text-gray-600"
+                        >
+                            Phone Number
+                        </label>
+                        <input
+                            type="text"
+                            name="category"
+                            id="category"
+                            class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                            placeholder="0123456789"
+                        />
+                        </div>
+                    </div>
+                    <div class="py-2 grid grid-cols-1">
+                        <div class="mt-1 px-2">
+                        <label
+                        for="category"
+                        class="block text-md font-medium text-gray-600"
+                        >
+                            Password
+                        </label>
+                        <button class="bg-black hover:bg-slate-600 text-white font-bold py-2 px-4 rounded"
+                         onClick={() => navigateTo('/passwordUpdate')}>
+                            Update Password
+                        </button>
+                        </div>
+                    </div>
+                </div>
+                <div class="py-2 mt-1 px-2">
+                    <button class="w-full bg-black hover:bg-slate-600 text-white font-bold py-2 px-4 rounded">
+                        Save Changes
+                    </button>
+                </div>
+            </div>
+        </div>
     </div>
     )
 }
