ab6e052eedd5a79738b99a9f660586f78ad32a34
SoSa-HEIDI <sonu@heidi-app.de>
2023-01-17 09:03:52 +0530

    created the basic UI outline of login and register pages

--- src/Path/Register.jsx
+++ src/Path/Register.jsx
@@ -1,11 +1,135 @@
-import React from 'react';
-  
-const Register =() =>{
-    return (
-    <div>
-        <h1>Registration Page</h1>
-        <p>This is the Registration Page</p>
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import HEIDI_Logo from "../Resource/HEIDI_Logo.png";
+
+const Register = () => {
+
+    let navigate  = useNavigate();  
+    const routeChangeToLogin   = () =>{ 
+        let path = `/`; 
+        navigate(path);
+      } 
+  return (
+    <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div class="w-full max-w-md space-y-8">
+        <div>
+          <img
+            class="mx-auto h-20 w-auto"
+            src={HEIDI_Logo}
+            alt="Your Company"
+          />
+          <h3 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+            Create your new account
+          </h3>
+        </div>
+        <form class="mt-8 space-y-6" action="#" method="POST">
+          <input type="hidden" name="remember" value="true" />
+          <div class="-space-y-px rounded-md shadow-sm">
+            <div>
+              <label for="username" class="sr-only">
+                Username
+              </label>
+              <input
+                id="user-name"
+                name="username"
+                type="text"
+                autocomplete="on"
+                required
+                class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                placeholder="Username"
+              />
+            </div>
+            <br/>
+            <div>
+              <label for="email-address" class="sr-only">
+                Email address
+              </label>
+              <input
+                id="email-address"
+                name="email"
+                type="email"
+                autocomplete="email"
+                required
+                class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                placeholder="Email address"
+              />
+            </div>
+            <br/>
+            <div>
+              <label for="password" class="sr-only">
+                Password
+              </label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                autocomplete="current-password"
+                required
+                class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                placeholder="Password"
+              />
+            </div>
+          </div>
+          
+          <div class="flex items-center justify-between">
+           <p>
+            <div class="flex items-center">
+              <input
+                id="remember-me"
+                name="remember-me"
+                type="checkbox"
+                class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+              />
+              <label for="remember-me" class="ml-2 block text-sm text-gray-900">
+                I accept the <a href="https://dev.heidi-app.de/cms/datenschutz/" target='_blank' rel="noopener noreferrer" class="text-red-500 hover:text-red-300">privacy policy*</a>
+              </label>
+            </div>
+            <br/>
+            <div class="flex items-center">
+              <input
+                id="remember-me"
+                name="remember-me"
+                type="checkbox"
+                class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+              />
+              <label for="remember-me" class="ml-2 block text-sm text-gray-900">
+                I hereby accept the <a href="https://dev.heidi-app.de/cms/datenschutz/" class="text-red-500 hover:text-red-300">terms and conditions*</a>
+              </label>
+            </div>
+           </p>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              onClick={routeChangeToLogin}
+              class="group relative flex w-full justify-center rounded-md border border-transparent bg-black py-2 px-4 text-sm font-medium text-white hover:text-slate-400 focus:outline-none focus:ring-2 focus:text-gray-400 focus:ring-offset-2"
+            >
+              <span class="absolute inset-y-0 left-0 flex items-center pl-3">
+                <svg
+                  class="h-5 w-5 text-gray-50 group-hover:text-slate-400"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  aria-hidden="true"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              </span>
+              Register
+            </button>
+          </div>
+          <div class="text-sm">
+        Already have an account? Please Login   
+        <span onClick={routeChangeToLogin} class="font-medium cursor-pointer text-black hover:text-gray-500"> here </span>
+        </div>
+        </form>
+      </div>
     </div>
-    )
-}
+  );
+};
 export default Register;
