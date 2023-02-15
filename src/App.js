f84286ae738c61c174843af1f4bca9d6db1d37fb
AkSu-HEIDI <akshay@heidi-app.de>
2023-02-15 10:43:00 +0530

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -6,11 +6,36 @@ import LoginPage from './Path/LoginPage';
 import Navigation from './Path/Navigation';
 import Dashboard from './Path/Dashboard';
 import Register from './Path/Register';
-import ListingsPage from './Path/ListingsPage';
+
+import HomePage from './Path/HomePage';
+import OverviewPage from './Path/Listings/OverviewPage';
+import OverviewPageNewsCategories from './Path/Listings/OverviewPageNewsCategories';
+import ListingsPageNewcitizeninfo from './Path/Listings/PageNewcitizeninfo';
+import ListingsPageEvents from './Path/Listings/PageEvents';
+import ListingsPageConstructionTraffic from './Path/Listings/PageConstructionTraffic';
+import ListingsPageSocieties from './Path/Listings/PageSocieties';
+import ListingsPageRegionalProducts from './Path/Listings/PageRegionalProducts';
+import ListingsPageOfferSearch from './Path/Listings/PageOfferSearch';
+import ListingsPageDefectReporter from './Path/Listings/PageDefectReporter';
+import ListingsPageLostPropertyOffice from './Path/Listings/PageLostPropertyOffice';
+
+import ListingsPageNewsflash from './Path/Listings/PageNewsflash';
+import ListingsPageClub from './Path/Listings/PageClub';
+import ListingsPageCompanyportaits from './Path/Listings/PageCompanyportaits';
+import ListingsPageOffers from './Path/Listings/PageOffers';
+import ListingsPageSports from './Path/Listings/PageSports';
+import ListingsPageAlert from './Path/Listings/PageAlert';
+import ListingsPagePolitics from './Path/Listings/PagePolitics';
+import ListingsPageEconomy from './Path/Listings/PageEconomy';
+import ListingsPageTopicOfTheDay from './Path/Listings/PageTopicOfTheDay';
+import ListingsPageLocal from './Path/Listings/PageLocal';
+import ListingsPageClubNews from "./Path/Listings/PageClubNews";
+
 import PasswordForgot from './Path/PasswordForgot';
 import PasswordUpdate from './Path/PasswordUpdate';
 import HEIDI_Logo from "./Resource/HEIDI_Logo.png";
-import "./i18n";
+
+//import OpenStreetMap from './Components/OpenStreetMap';
 
 const App =()=>{
   useEffect(() => {
@@ -25,14 +50,39 @@ const App =()=>{
       <div>
         <Navigation />
           <Routes>
+          <Route path="/HomePage" element={<HomePage />}/>
             <Route path="/Dashboard" element={<Dashboard />} exact/>
             <Route path="/ProfilePage" element={<ProfilePage />}/>
             <Route path="/PasswordForgot" element={<PasswordForgot />}/>
             <Route path="/PasswordUpdate" element={<PasswordUpdate />}/>
             <Route path="/" element={<LoginPage />}/>
             <Route path="/Register" element={<Register />}/>
-            <Route path="/ListingsPage" element={<ListingsPage />}/>
-             <Route path='*' element={<Error />}/>
+
+            <Route path="/OverviewPage" element={<OverviewPage />}/>
+            <Route path="/OverviewPageNewsCategories" element={<OverviewPageNewsCategories />}/>
+            <Route path="/ListingsPageNewcitizeninfo" element={<ListingsPageNewcitizeninfo />}/>
+            <Route path="/ListingsPageEvents" element={<ListingsPageEvents />}/>
+            <Route path="/ListingsPageConstructionTraffic" element={<ListingsPageConstructionTraffic />}/>
+            <Route path="/ListingsPageSocieties" element={<ListingsPageSocieties />}/>
+            <Route path="/ListingsPageRegionalProducts" element={<ListingsPageRegionalProducts />}/>
+            <Route path="/ListingsPageOfferSearch" element={<ListingsPageOfferSearch />}/>
+            <Route path="/ListingsPageDefectReporter" element={<ListingsPageDefectReporter />}/>
+            <Route path="/ListingsPageLostPropertyOffice" element={<ListingsPageLostPropertyOffice />}/>
+            <Route path="/ListingsPageNewsflash" element={<ListingsPageNewsflash />}/>
+            <Route path="/ListingsPageClub" element={<ListingsPageClub />}/>
+            <Route path="/ListingsPageCompanyportaits" element={<ListingsPageCompanyportaits />}/>
+            <Route path="/ListingsPageOffers" element={<ListingsPageOffers />}/>
+            <Route path="/ListingsPageSports" element={<ListingsPageSports />}/>
+            <Route path="/ListingsPageAlert" element={<ListingsPageAlert />}/>
+            <Route path="/ListingsPagePolitics" element={<ListingsPagePolitics />}/>
+            <Route path="/ListingsPageEconomy" element={<ListingsPageEconomy />}/>
+            <Route path="/ListingsPageClubNews" element={<ListingsPageClubNews />}/>
+            <Route path="/ListingsPageTopicOfTheDay" element={<ListingsPageTopicOfTheDay />}/>
+            <Route path="/ListingsPageLocal" element={<ListingsPageLocal />}/>
+
+            {/* <Route path="/OpenStreetMap" element={<OpenStreetMap />}/> */}
+
+            <Route path='*' element={<Error />}/>
           </Routes>
       </div>
     </BrowserRouter>
