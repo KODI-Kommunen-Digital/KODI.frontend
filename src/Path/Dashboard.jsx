e19deef303a763ecd46bd32dda21816d686a19b3
SoSa-HEIDI <sonu@heidi-app.de>
2023-05-23 09:29:38 +0200

    added city images and names

--- src/Path/Dashboard.jsx
+++ src/Path/Dashboard.jsx
@@ -344,20 +344,20 @@ const Dashboard = () => {
 															</svg>
 														</div>
 														<div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-															<h3 className="text-lg leading-6 font-medium text-gray-900">Are you sure?</h3>
+															<h3 className="text-lg leading-6 font-medium text-gray-900">{t("areyousure")}</h3>
 															<div className="mt-2">
-															<p className="text-sm text-gray-500">Do you really want to delete this listing? This action cannot be undone.</p>
+															<p className="text-sm text-gray-500">{t("doyoureallywanttodeleteListing")}</p>
 															</div>
 														</div>
 														</div>
 													</div>
 													<div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
 													<button onClick={showConfirmationModal.onConfirm} type="button" className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm">
-														Delete
+														{t("delete")}
 													</button>
 
 													<button onClick={showConfirmationModal.onCancel} type="button" className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:w-auto sm:text-sm">
-														Cancel
+														{t("cancel")}
 													</button>
 
 													</div>
