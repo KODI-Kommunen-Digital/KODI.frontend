433ff67d12886c1033d06f306ed296d3e7f7e6ad
Spoorthi Madhavan <madhavan@mail.uni-paderborn.de>
2023-05-25 09:22:11 +0200

    Testing as the issue is only in server

--- src/Path/SubPages/EventDetails.js
+++ src/Path/SubPages/EventDetails.js
@@ -187,18 +187,18 @@ const EventDetails = () => {
 
 
 	useEffect(() => {
-		// const searchParams = new URLSearchParams(window.location.search);
-		// var cityId = searchParams.get("cityId");
-		// setCityId(cityId);
+		const searchParams = new URLSearchParams(window.location.search);
+		var cityId = searchParams.get("cityId");
+		setCityId(cityId);
 		if (selectedCategoryId) {
-		getListings({ categoryId: selectedCategoryId,statusId:1,cityId:cityId }).then((response) => {
+		getListings({ categoryId: selectedCategoryId,statusId:1 }).then((response) => {
 			const filteredListings = response.data.data.filter(
 			(listing) => listing.id !== listingId
 			);
 			setListings(filteredListings);
 		});
 		}
-	}, [selectedCategoryId, listingId]);
+	}, [selectedCategoryId, listingId, cityId]);
 
 
 	const [selectedSortOption, setSelectedSortOption] = useState("");
