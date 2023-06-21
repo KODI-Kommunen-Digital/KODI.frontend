5b7a9d8defaa22b00128d05716d4932869da8c8f
AkSu-HEIDI <akshay@heidi-app.de>
2023-06-21 09:10:00 +0200

    Logout of all device

--- src/Path/AllDevices.jsx
+++ src/Path/AllDevices.jsx
@@ -1,29 +1,26 @@
-import React, { useState, useEffect, Fragment } from "react";
+import React, { useState, useEffect } from "react";
 import SideBar from "../Components/SideBar";
 import { useTranslation } from "react-i18next";
 import "../index.css";
 import axios from "axios";
 import DEVICEIMAGE from "../assets/computer-tablet-phone-icon.png";
+import {
+	logout,
+	fetchDeviceList,
+	logoutOfAllDevices,
+	logoutOfOneDevice,
+} from "../Services/usersApi";
 
 const AllDevices = () => {
 	const { t } = useTranslation();
 	const [devices, setDevices] = useState([]);
 
 	useEffect(() => {
-		fetchDeviceList();
+		fetchDeviceList().then((response) => {
+			setDevices(response.data.data);
+		});
 	}, []);
-
-	const fetchDeviceList = async () => {
-		try {
-			const response = await axios.get(
-				`${process.env.REACT_APP_API_BASE_URL}/users/devices`
-			);
-			const deviceList = response.data.data.devices;
-			setDevices(deviceList);
-		} catch (error) {
-			console.error("Error fetching device list:", error);
-		}
-	};
+	console.log(devices);
 
 	return (
 		<section className="bg-slate-600 body-font relative h-screen">
@@ -49,19 +46,63 @@ const AllDevices = () => {
 							</div>
 						</div>
 
-						{devices && Array.isArray(devices) && devices.length > 0 ? (
-							<ul>
+						{devices && devices.length > 0 ? (
+							<>
 								{devices.map((device) => (
-									<li key={device.id}>{device.deviceName}</li>
+									<div
+										key={device.id}
+										className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-slate-600 text-black"
+									>
+										<svg
+											className="h-6 w-10 fill-current"
+											xmlns="http://www.w3.org/2000/svg"
+											viewBox="0 0 496 512"
+										>
+											<path d="M64 0C28.7 0 0 28.7 0 64V352c0 35.3 28.7 64 64 64H240l-10.7 32H160c-17.7 0-32 14.3-32 32s14.3 32 32 32H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H346.7L336 416H512c35.3 0 64-28.7 64-64V64c0-35.3-28.7-64-64-64H64zM512 64V352H64V64H512z" />
+										</svg>
+										<span
+											className="text-[15px] ml-4 text-black font-bold"
+											style={{ fontFamily: "Poppins, sans-serif" }}
+										>
+											{device.device} {device.browser}
+										</span>
+									</div>
 								))}
-							</ul>
+							</>
 						) : (
-							<p
-								className="text-gray-900 text-md mb-4 font-medium title-font"
-								style={{ fontFamily: "Poppins, sans-serif" }}
-							>
-								Loading devices...
-							</p>
+							<div>
+								<div className="flex items-center justify-center">
+									<h1
+										className="m-auto mt-20 text-center font-sans font-bold text-2xl text-black"
+										style={{ fontFamily: "Poppins, sans-serif" }}
+									>
+										{t("currently_no_groups")}
+									</h1>
+								</div>
+								<div className="m-auto mt-10 mb-40 text-center font-sans font-bold text-xl">
+									<span
+										className="font-sans text-black"
+										style={{ fontFamily: "Poppins, sans-serif" }}
+									>
+										{t("to_create_new_group")}
+									</span>
+									<a
+										className="m-auto mt-20 text-center font-sans font-bold text-xl cursor-pointer text-black"
+										style={{ fontFamily: "Poppins, sans-serif" }}
+										onClick={() => {
+											localStorage.setItem(
+												"selectedItem",
+												"Choose one category"
+											);
+											isLoggedIn
+												? navigateTo("/UploadListings")
+												: navigateTo("/login");
+										}}
+									>
+										{t("click_here")}
+									</a>
+								</div>
+							</div>
 						)}
 
 						<button
--- src/Path/CreateGroup.jsx
+++ src/Path/CreateGroup.jsx
@@ -6,7 +6,6 @@ import { useTranslation } from "react-i18next";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import {
-	getForumsById,
 	postForumsData,
 	updateForumsData,
 	uploadImage,
@@ -16,7 +15,6 @@ import { getCities } from "../Services/cities";
 import { getVillages } from "../Services/villages";
 import FormData from "form-data";
 import Alert from "../Components/Alert";
-import { categoryByName, categoryById } from "../Constants/categories";
 
 function CreateGroup() {
 	const { t } = useTranslation();
@@ -58,7 +56,7 @@ function CreateGroup() {
 						if (filePath?.data?.status === "success") {
 							setInput((prevInput) => ({
 								...prevInput,
-								logo: filePath?.data?.path || null,
+								image: filePath?.data?.path || null,
 								removeImage: false,
 							}));
 						} else {
@@ -130,25 +128,17 @@ function CreateGroup() {
 
 	function handleRemoveImage1() {
 		setImage1(null);
-		setInput((prevInput) => ({ ...prevInput, logo: null, removeImage: true }));
+		setInput((prevInput) => ({ ...prevInput, image: null, removeImage: true }));
 	}
 
 	//Sending data to backend starts
 	const [val, setVal] = useState([{ socialMedia: "", selected: "" }]);
 	const [input, setInput] = useState({
-		categoryId: 0,
-		subcategoryId: 0,
-		cityId: 0,
-		statusId: 3,
-		sourceId: 1,
-		userId: 2,
 		title: "",
 		description: "",
-		logo: null,
-		pdf: null,
-		villagedropdown: "",
+		image: null,
 		removeImage: false,
-		visibility: "public", // Add the visibility property
+		visibility: "public",
 	});
 	console.log(input);
 
@@ -163,6 +153,8 @@ function CreateGroup() {
 	});
 
 	const handleSubmit = async (event) => {
+		event.preventDefault();
+
 		let valid = true;
 		for (let key in error) {
 			var errorMessage = getErrorMessage(key, input[key]);
@@ -173,14 +165,16 @@ function CreateGroup() {
 				valid = false;
 			}
 		}
+
 		if (valid) {
 			setUpdating(true);
-			event.preventDefault();
 			try {
+				input.isPrivate = input.visibility == "private";
 				var response = newListing
 					? await postForumsData(cityId, input)
-					: await updateForumsData(cityId, input, listingId);
-				setSuccessMessage(t("listingUpdated"));
+					: await updateForumsData(cityId, input, forumsId);
+
+				setSuccessMessage(t("groupCreated"));
 				setErrorMessage(false);
 				setTimeout(() => {
 					setSuccessMessage(false);
@@ -313,45 +307,9 @@ function CreateGroup() {
 					return "";
 				}
 
-			case "categoryId":
-				if (!parseInt(value)) {
-					return t("pleaseSelectCategory");
-				} else {
-					return "";
-				}
-
-			case "subCategoryId":
-				if (!value && parseInt(input.categoryId) == categoryByName.news) {
-					return t("pleaseSelectSubcategory");
-				} else {
-					return "";
-				}
-
 			case "description":
 				if (!value) {
 					return t("pleaseEnterDescription");
-				} else {
-					return "";
-				}
-
-			case "startDate":
-				if (!value && parseInt(input.categoryId) == categoryByName.events) {
-					return t("pleaseEnterStartDate");
-				} else {
-					return "";
-				}
-
-			case "endDate":
-				if (parseInt(input.categoryId) == categoryByName.events) {
-					if (!value) {
-						return t("pleaseEnterEndDate");
-					} else {
-						if (new Date(input.startDate) > new Date(value)) {
-							return t("endDateBeforeStartDate");
-						} else {
-							return "";
-						}
-					}
 				} else {
 					return "";
 				}
@@ -398,6 +356,11 @@ function CreateGroup() {
 		if (parseInt(cityId))
 			getVillages(cityId).then((response) => setVillages(response.data.data));
 		validateInput(e);
+
+		const urlParams = new URLSearchParams(window.location.search);
+		urlParams.set("cityId", cityId);
+		const newUrl = `${window.location.pathname}?${urlParams.toString()}`;
+		window.history.replaceState({}, "", newUrl);
 	}
 	const [categoryId, setCategoryId] = useState(0);
 
@@ -505,7 +468,7 @@ function CreateGroup() {
 								<div
 									className={`w-10 h-6 rounded-full shadow-inner ${
 										input.visibility === "private"
-											? "bg-green-500"
+											? "bg-blue-500"
 											: "bg-gray-300"
 									}`}
 								></div>
@@ -590,14 +553,14 @@ function CreateGroup() {
 							onDragEnter={handleDragEnter}
 							onDragLeave={handleDragLeave}
 						>
-							{image1 || input.logo ? (
+							{image1 || input.image ? (
 								<div className="flex flex-col items-center">
 									<img
 										className="object-contain h-64 w-full mb-4"
 										src={
 											image1
 												? URL.createObjectURL(image1)
-												: process.env.REACT_APP_BUCKET_HOST + input.logo
+												: process.env.REACT_APP_BUCKET_HOST + input.image
 										}
 										alt="uploaded"
 									/>
@@ -653,24 +616,6 @@ function CreateGroup() {
 							class="w-full bg-black hover:bg-slate-600 text-white font-bold py-2 px-4 rounded disabled:opacity-60"
 						>
 							{t("saveChanges")}
-							{updating && (
-								<svg
-									aria-hidden="true"
-									class="inline w-5 h-5 ml-2 text-gray-200 animate-spin dark:text-gray-600 fill-gray-600 dark:fill-gray-300"
-									viewBox="0 0 100 101"
-									fill="none"
-									xmlns="http://www.w3.org/2000/svg"
-								>
-									<path
-										d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-										fill="currentColor"
-									/>
-									<path
-										d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-										fill="currentFill"
-									/>
-								</svg>
-							)}
 						</button>
 					</div>
 					<div>
