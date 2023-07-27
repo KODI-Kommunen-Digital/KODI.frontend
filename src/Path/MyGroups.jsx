0b650b6f50c19fcf72511e85590122a42f70e176
AkSu-HEIDI <akshay@heidi-app.de>
2023-07-27 11:48:23 +0200

    Forums Changes

--- src/Path/MyGroups.jsx
+++ src/Path/MyGroups.jsx
@@ -3,7 +3,7 @@ import SideBar from "../Components/SideBar";
 import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import "../index.css";
-import { getUserForums } from "../Services/forumsApi";
+import { getUserForums, deleteForums } from "../Services/forumsApi";
 import { status } from "../Constants/status";
 import GROUPIMAGE from "../assets/GroupImage.avif";
 
@@ -24,7 +24,6 @@ const MyGroups = () => {
 		}
 		getUserForums().then((response) => {
 			setForums(response.data.data);
-			console.log(response.data.data);
 		});
 	}, []);
 	const [pageNo, setPageNo] = useState(1);
@@ -39,6 +38,37 @@ const MyGroups = () => {
 		if (status[statusId] === "Pending") {
 			return "bg-yellow-400";
 		}
+	}
+
+	function goToEditForumsPage(forum) {
+		navigateTo(`/CreateGroup?forumId=${forum.id}&cityId=${forum.cityId}`);
+	}
+
+	const [showConfirmationModal, setShowConfirmationModal] = useState({
+		visible: false,
+		forum: null,
+		onConfirm: () => {},
+		onCancel: () => {},
+	});
+
+	function handleDelete(forum) {
+		deleteForums(forum.cityId, forum.id)
+			.then((res) => {
+				setForums(
+					forums.filter((f) => f.cityId !== forum.cityId || f.id !== forum.id)
+				);
+				setShowConfirmationModal({ visible: false }); // hide the confirmation modal
+			})
+			.catch((error) => console.log(error));
+	}
+
+	function deleteForumsOnClick(forum) {
+		setShowConfirmationModal({
+			visible: true,
+			forum,
+			onConfirm: () => handleDelete(forum),
+			onCancel: () => setShowConfirmationModal({ visible: false }),
+		});
 	}
 
 	const navigate = useNavigate();
@@ -115,6 +145,13 @@ const MyGroups = () => {
 										className="px-6 py-3 text-center"
 										style={{ fontFamily: "Poppins, sans-serif" }}
 									>
+										{t("action")}
+									</th>
+									<th
+										scope="col"
+										className="px-6 py-3 text-center"
+										style={{ fontFamily: "Poppins, sans-serif" }}
+									>
 										{t("status")}
 									</th>
 								</tr>
@@ -182,6 +219,92 @@ const MyGroups = () => {
 													? "Private group"
 													: "Public group"}
 											</td>
+
+											<td className="px-6 py-4 text-center">
+												<a
+													className="font-medium text-blue-600 dark:text-blue-500 hover:underline cursor-pointer pr-2"
+													onClick={() => goToEditForumsPage(forum)}
+													style={{ fontFamily: "Poppins, sans-serif" }}
+												>
+													{t("edit")}
+												</a>
+												<a
+													className="font-medium text-blue-600 dark:text-blue-500 hover:underline cursor-pointer text-center"
+													onClick={() => deleteForumsOnClick(forum)}
+													style={{ fontFamily: "Poppins, sans-serif" }}
+												>
+													{t("delete")}
+												</a>
+												{showConfirmationModal.visible && (
+													<div className="fixed z-10 inset-0 overflow-y-auto">
+														<div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+															<div
+																className="fixed inset-0 transition-opacity"
+																aria-hidden="true"
+															>
+																<div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+															</div>
+															<span
+																className="hidden sm:inline-block sm:align-middle sm:h-screen"
+																aria-hidden="true"
+															>
+																&#8203;
+															</span>
+															<div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+																<div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+																	<div className="sm:flex sm:items-start">
+																		<div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+																			<svg
+																				className="h-6 w-6 text-red-600"
+																				xmlns="http://www.w3.org/2000/svg"
+																				fill="none"
+																				viewBox="0 0 24 24"
+																				stroke="currentColor"
+																				aria-hidden="true"
+																			>
+																				<path
+																					strokeLinecap="round"
+																					strokeLinejoin="round"
+																					strokeWidth="2"
+																					d="M6 18L18 6M6 6l12 12"
+																				/>
+																			</svg>
+																		</div>
+																		<div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+																			<h3 className="text-lg leading-6 font-medium text-gray-900">
+																				{t("areyousure")}
+																			</h3>
+																			<div className="mt-2">
+																				<p className="text-sm text-gray-500">
+																					{t("doyoureallywanttodeleteListing")}
+																				</p>
+																			</div>
+																		</div>
+																	</div>
+																</div>
+																<div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+																	<button
+																		onClick={showConfirmationModal.onConfirm}
+																		type="button"
+																		className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
+																	>
+																		{t("delete")}
+																	</button>
+
+																	<button
+																		onClick={showConfirmationModal.onCancel}
+																		type="button"
+																		className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:w-auto sm:text-sm"
+																	>
+																		{t("cancel")}
+																	</button>
+																</div>
+															</div>
+														</div>
+													</div>
+												)}
+											</td>
+
 											<td className="px-6 py-4">
 												<div className="flex items-center justify-center">
 													<div
