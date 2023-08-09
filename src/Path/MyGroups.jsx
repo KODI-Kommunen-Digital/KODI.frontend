79f460915bf8481b30b5574e53173f657e731727
AkSu-HEIDI <akshay@heidi-app.de>
2023-08-09 11:19:29 +0200

    Merging Issues Resolved For Forums.

--- src/Path/MyGroups.jsx
+++ src/Path/MyGroups.jsx
@@ -3,13 +3,18 @@ import SideBar from "../Components/SideBar";
 import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import "../index.css";
-import { getUserForums, deleteForums } from "../Services/forumsApi";
+import { getUserForums } from "../Services/forumsApi";
 import GROUPIMAGE from "../assets/GroupImage.avif";
 
 const MyGroups = () => {
 	const { t } = useTranslation();
-	const [forum, setForums] = useState([]);
-	const [, setIsLoggedIn] = useState(false);
+	const [forums, setForums] = useState([]);
+
+	useEffect(() => {
+		getUserForums().then((response) => {
+			setForums(response.data.data);
+		});
+	}, []);
 
 	const navigate = useNavigate();
 	const navigateTo = (path) => {
@@ -18,57 +23,13 @@ const MyGroups = () => {
 		}
 	};
 
-	useEffect(() => {
-		const accessToken =
-			window.localStorage.getItem("accessToken") ||
-			window.sessionStorage.getItem("accessToken");
-		const refreshToken =
-			window.localStorage.getItem("refreshToken") ||
-			window.sessionStorage.getItem("refreshToken");
-		if (accessToken || refreshToken) {
-			setIsLoggedIn(true);
-		}
-		getUserForums().then((response) => {
-			setForums(response.data.data);
-		});
-	}, []);
-	const [pageNo, setPageNo] = useState(1);
-
-	const [showConfirmationModal, setShowConfirmationModal] = useState({
-		visible: false,
-		forum: null,
-		onConfirm: () => {},
-		onCancel: () => {},
-	});
-
-	function handleDelete(forum) {
-		deleteForums(forum.cityId, forum.id)
-			.then((res) => {
-				setForums(
-					forum.filter((f) => f.cityId !== forum.cityId || f.id !== forum.id)
-				);
-				setShowConfirmationModal({ visible: false }); // hide the confirmation modal
-			})
-			.catch((error) => console.log(error));
-	}
-
-	function deleteForumsOnClick(forum) {
-		setShowConfirmationModal({
-			visible: true,
-			forum,
-			onConfirm: () => handleDelete(forum),
-			onCancel: () => setShowConfirmationModal({ visible: false }),
-		});
-	}
-
 	return (
 		<section className="bg-slate-600 body-font relative h-screen">
 			<SideBar />
-
 			<div className="container w-auto px-0 lg:px-5 py-2 bg-slate-600 min-h-screen flex flex-col">
 				<div className="h-full">
 					<div className="bg-white mt-10 p-0 space-y-10 overflow-x-auto">
-						<table className="w-full text-sm text-left lg:mt-[2rem] mt-[2rem] text-gray-500 dark:text-gray-400 p-6 space-y-10 rounded-xl">
+						<table className="w-full text-sm text-left lg:mt-[2rem] mt-[2rem] text-gray-500 dark:text-gray-400 p-6 space-y-10 rounded-lg">
 							<thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-50 dark:text-gray-700">
 								<tr>
 									<th
@@ -123,24 +84,10 @@ const MyGroups = () => {
 									>
 										{t("privacy")}
 									</th>
-									<th
-										scope="col"
-										className="px-6 py-3 text-center"
-										style={{ fontFamily: "Poppins, sans-serif" }}
-									>
-										{t("action")}
-									</th>
-									{/* <th
-										scope="col"
-										className="px-6 py-3 text-center"
-										style={{ fontFamily: "Poppins, sans-serif" }}
-									>
-										{t("status")}
-									</th> */}
 								</tr>
 							</thead>
 							<tbody>
-								{forum.map((forum, index) => {
+								{forums.map((forum, index) => {
 									return (
 										<tr
 											key={index}
@@ -161,8 +108,13 @@ const MyGroups = () => {
 												/>
 												<div className="pl-0 sm:pl-3 overflow-hidden max-w-[20rem] sm:max-w-[10rem]">
 													<div
-														className="font-normal text-gray-500 truncate"
+														className="font-medium text-blue-600 dark:text-blue-500 hover:underline cursor-pointer text-center"
 														style={{ fontFamily: "Poppins, sans-serif" }}
+														onClick={() =>
+															navigateTo(
+																`/Forum?forumId=${forum.forumId}&cityId=${forum.cityId}`
+															)
+														}
 													>
 														{forum.forumName}
 													</div>
@@ -174,7 +126,7 @@ const MyGroups = () => {
 												style={{ fontFamily: "Poppins, sans-serif" }}
 												onClick={() =>
 													navigateTo(
-														`/MyGroups/GroupMembers?id=${forum.forumId}&cityId=${forum.cityId}`
+														`/MyGroups/GroupMembers?forumId=${forum.forumId}&cityId=${forum.cityId}`
 													)
 												}
 											>
@@ -202,129 +154,11 @@ const MyGroups = () => {
 													? "Private group"
 													: "Public group"}
 											</td>
-
-											<td className="px-6 py-4 text-center">
-												<a
-													className="font-medium text-blue-600 dark:text-blue-500 hover:underline cursor-pointer pr-2"
-													onClick={() =>
-														navigateTo(
-															`/CreateGroup?forumId=${forum.forumId}&cityId=${forum.cityId}`
-														)
-													}
-													style={{ fontFamily: "Poppins, sans-serif" }}
-												>
-													{t("edit")}
-												</a>
-												<a
-													className="font-medium text-blue-600 dark:text-blue-500 hover:underline cursor-pointer text-center"
-													onClick={() => deleteForumsOnClick(forum)}
-													style={{ fontFamily: "Poppins, sans-serif" }}
-												>
-													{t("delete")}
-												</a>
-												{showConfirmationModal.visible && (
-													<div className="fixed z-50 inset-0 overflow-y-auto">
-														<div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-															<div
-																className="fixed inset-0 transition-opacity"
-																aria-hidden="true"
-															>
-																<div className="absolute inset-0 bg-gray-500 opacity-75"></div>
-															</div>
-															<span
-																className="hidden sm:inline-block sm:align-middle sm:h-screen"
-																aria-hidden="true"
-															>
-																&#8203;
-															</span>
-															<div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-																<div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-																	<div className="sm:flex sm:items-start">
-																		<div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-																			<svg
-																				className="h-6 w-6 text-red-600"
-																				xmlns="http://www.w3.org/2000/svg"
-																				fill="none"
-																				viewBox="0 0 24 24"
-																				stroke="currentColor"
-																				aria-hidden="true"
-																			>
-																				<path
-																					strokeLinecap="round"
-																					strokeLinejoin="round"
-																					strokeWidth="2"
-																					d="M6 18L18 6M6 6l12 12"
-																				/>
-																			</svg>
-																		</div>
-																		<div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-																			<h3 className="text-lg leading-6 font-medium text-gray-900">
-																				{t("areyousure")}
-																			</h3>
-																			<div className="mt-2">
-																				<p className="text-sm text-gray-500">
-																					{t("doyoureallywanttodeleteListing")}
-																				</p>
-																			</div>
-																		</div>
-																	</div>
-																</div>
-																<div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-																	<button
-																		onClick={showConfirmationModal.onConfirm}
-																		type="button"
-																		className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
-																	>
-																		{t("delete")}
-																	</button>
-
-																	<button
-																		onClick={showConfirmationModal.onCancel}
-																		type="button"
-																		className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:w-auto sm:text-sm"
-																	>
-																		{t("cancel")}
-																	</button>
-																</div>
-															</div>
-														</div>
-													</div>
-												)}
-											</td>
 										</tr>
 									);
 								})}
 							</tbody>
 						</table>
-					</div>
-					<div className="bottom-5 right-5 mt-5 px-1 py-2 text-xs font-medium text-center float-right cursor-pointer bg-black rounded-xl">
-						{pageNo !== 1 ? (
-							<span
-								className="inline-block bg-black px-2 pb-2 pt-2 text-xs font-bold uppercase leading-normal text-neutral-50 transition duration-150 ease-in-out focus:bg-neutral-800 focus:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:outline-none focus:ring-0 active:bg-neutral-900 active:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] dark:bg-neutral-900 dark:shadow-[0_4px_9px_-4px_#030202] dark:hover:bg-neutral-900 dark:hover:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)] dark:focus:bg-neutral-900 dark:focus:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)] dark:active:bg-neutral-900 dark:active:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)]"
-								onClick={() => setPageNo(pageNo - 1)}
-								style={{ fontFamily: "Poppins, sans-serif" }}
-							>
-								{"<"}{" "}
-							</span>
-						) : (
-							<span />
-						)}
-						<span
-							className="inline-block bg-black px-2 pb-2 pt-2 text-xs font-bold uppercase leading-normal text-neutral-50 transition duration-150 ease-in-out focus:bg-neutral-800 focus:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:outline-none focus:ring-0 active:bg-neutral-900 active:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] dark:bg-neutral-900 dark:shadow-[0_4px_9px_-4px_#030202] dark:hover:bg-neutral-900 dark:hover:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)] dark:focus:bg-neutral-900 dark:focus:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)] dark:active:bg-neutral-900 dark:active:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)]"
-							style={{ fontFamily: "Poppins, sans-serif" }}
-						>
-							{t("page")} {pageNo}
-						</span>
-
-						{forum.length >= 9 && (
-							<span
-								className="inline-block bg-black px-2 pb-2 pt-2 text-xs font-bold uppercase leading-normal text-neutral-50 transition duration-150 ease-in-out focus:bg-neutral-800 focus:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] focus:outline-none focus:ring-0 active:bg-neutral-900 active:shadow-[0_8px_9px_-4px_rgba(51,45,45,0.2),0_4px_18px_0_rgba(51,45,45,0.1)] dark:bg-neutral-900 dark:shadow-[0_4px_9px_-4px_#030202] dark:hover:bg-neutral-900 dark:hover:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)] dark:focus:bg-neutral-900 dark:focus:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)] dark:active:bg-neutral-900 dark:active:shadow-[0_8px_9px_-4px_rgba(3,2,2,0.3),0_4px_18px_0_rgba(3,2,2,0.2)]"
-								onClick={() => setPageNo(pageNo + 1)}
-								style={{ fontFamily: "Poppins, sans-serif" }}
-							>
-								{">"}
-							</span>
-						)}
 					</div>
 				</div>
 			</div>
