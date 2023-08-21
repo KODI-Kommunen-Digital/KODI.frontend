9de6da367719cfc8057255379fe9884ce45c7631
AkSu-HEIDI <akshay@heidi-app.de>
2023-08-21 16:02:41 +0200

    Loading Screen Added

--- src/Path/CitizenServices/AllForums.jsx
+++ src/Path/CitizenServices/AllForums.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import HomePageNavBar from "../../Components/HomePageNavBar";
 import LISTINGSIMAGE from "../../assets/ListingsImage.jpeg";
 import { useTranslation } from "react-i18next";
+import LoadingPage from "../../Path/LoadingPage";
 import { getAllForums, getUserForums } from "../../Services/forumsApi";
 import { getCities } from "../../Services/cities";
 import Footer from "../../Components/Footer";
@@ -19,6 +20,7 @@ const AllForums = () => {
 	const [isLoggedIn, setIsLoggedIn] = useState(false);
 	const [isValidForum, setIsValidForum] = useState(false);
 	const navigate = useNavigate();
+	const [isLoading, setIsLoading] = useState(true);
 
 	useEffect(() => {
 		const urlParams = new URLSearchParams(window.location.search);
@@ -40,16 +42,18 @@ const AllForums = () => {
 
 	useEffect(() => {
 		const urlParams = new URLSearchParams(window.location.search);
-		const params = { pageNo, pageSize: 9, statusId: 1 };
+		const params = { pageNo, pageSize: 12, statusId: 1 };
+		setIsLoading(true);
 		const cityHasForum =
 			cities.find((city) => city.id === parseInt(cityId))?.hasForum === 1;
+		console.log("cityHasForum:", cities);
+
 		if (parseInt(cityId)) {
 			setCityName(cities.find((c) => parseInt(cityId) === c.id)?.name);
 			params.cityId = cityId;
 			setIsValidForum(cityHasForum);
 		} else {
 			setCityName(t("allCities"));
-			// urlParams.delete("cityId");
 			setIsValidForum(false);
 		}
 		const newUrl = `${window.location.pathname}?${urlParams.toString()}`;
@@ -61,10 +65,26 @@ const AllForums = () => {
 				setUserForums(data);
 			});
 		}
-		getAllForums(cityId).then((response) => {
-			const data = response.data.data;
-			setForums(data);
-		});
+		const fetchData = async () => {
+			try {
+				const response = await getAllForums(cityId);
+				const data = response.data.data;
+				console.log(data);
+				setForums(data);
+			} catch (error) {
+				console.error("Error fetching forums:", error);
+			} finally {
+				setIsLoading(false);
+			}
+		};
+
+		const fetchDataWithDelay = () => {
+			setTimeout(() => {
+				fetchData();
+			}, 2000);
+		};
+
+		fetchDataWithDelay();
 	}, [cities, cityId, pageNo, t, isLoggedIn]);
 
 	const checkIfMember = (forumId) => {
@@ -179,45 +199,48 @@ const AllForums = () => {
 					</div>
 
 					<div className="mt-5 mb-20 p-6">
-						<div>
-							{forums && forums.length > 0 ? (
-								<div className="bg-white lg:px-10 md:px-5 sm:px-0 px-2 py-6 mt-10 mb-10 space-y-10 flex flex-col">
-									<div className="relative place-items-center bg-white mt-4 mb-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4 gap-10 justify-start">
-										{forums &&
-											forums.map((forum) => (
-												<div
-													key={forum.id}
-													onClick={() => handleClick(cityId, forum)}
-													className="w-full h-full shadow-lg rounded-lg cursor-pointer"
-												>
-													<a className="block relative h-64 rounded overflow-hidden">
-														{checkIfMember(forum.id) && forum.isPrivate ? (
-															<>
-																<img
-																	alt="forumImage"
-																	className="object-cover object-center w-full h-full block hover:scale-125 transition-all duration-1000"
-																	src={
-																		forum.image
-																			? process.env.REACT_APP_BUCKET_HOST +
+						{isLoading ? (
+							<LoadingPage />
+						) : (
+							<div>
+								{forums && forums.length > 0 ? (
+									<div className="bg-white lg:px-10 md:px-5 sm:px-0 px-2 py-6 mt-10 mb-10 space-y-10 flex flex-col">
+										<div className="relative place-items-center bg-white mt-4 mb-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4 gap-10 justify-start">
+											{forums &&
+												forums.map((forum) => (
+													<div
+														key={forum.id}
+														onClick={() => handleClick(cityId, forum)}
+														className="w-full h-full shadow-lg rounded-lg cursor-pointer"
+													>
+														<a className="block relative h-64 rounded overflow-hidden">
+															{checkIfMember(forum.id) && forum.isPrivate ? (
+																<>
+																	<img
+																		alt="forumImage"
+																		className="object-cover object-center w-full h-full block hover:scale-125 transition-all duration-1000"
+																		src={
 																			forum.image
-																			: LISTINGSIMAGE
-																	}
-																/>
-															</>
-														) : (
-															<>
-																<img
-																	alt="forumImage"
-																	className="object-cover object-center w-full h-full block hover:scale-125 transition-all duration-1000"
-																	src={
-																		forum.image
-																			? process.env.REACT_APP_BUCKET_HOST +
+																				? process.env.REACT_APP_BUCKET_HOST +
+																				  forum.image
+																				: LISTINGSIMAGE
+																		}
+																	/>
+																</>
+															) : (
+																<>
+																	<img
+																		alt="forumImage"
+																		className="object-cover object-center w-full h-full block hover:scale-125 transition-all duration-1000"
+																		src={
 																			forum.image
-																			: LISTINGSIMAGE
-																	}
-																/>
+																				? process.env.REACT_APP_BUCKET_HOST +
+																				  forum.image
+																				: LISTINGSIMAGE
+																		}
+																	/>
 
-																{/* <div className="absolute inset-0 flex flex-col justify-center items-center bg-gray-800 bg-opacity-50 text-white">
+																	{/* <div className="absolute inset-0 flex flex-col justify-center items-center bg-gray-800 bg-opacity-50 text-white">
 															<div className="border border-white p-4">
 																<h1
 																	className="text-lg md:text-lg font-sans font-bold mb-0"
@@ -229,72 +252,73 @@ const AllForums = () => {
 																</h1>
 															</div>
 														</div> */}
-															</>
-														)}
-													</a>
-													<div className="mt-5 px-2">
-														<div
-															className="flex justify-between items-center"
-															style={{ fontFamily: "Poppins, sans-serif" }}
-														>
-															<h2 className="text-gray-900 title-font text-md font-bold font-sans truncate">
-																{forum.forumName}
-															</h2>
-															{forum.isPrivate === 0 ? (
-																<h2 className="text-blue-400 title-font text-md font-bold font-sans truncate">
-																	Public
-																</h2>
-															) : (
-																<h2 className="text-blue-400 title-font text-md font-bold font-sans truncate">
-																	Private
-																</h2>
+																</>
 															)}
+														</a>
+														<div className="mt-5 px-2">
+															<div
+																className="flex justify-between items-center"
+																style={{ fontFamily: "Poppins, sans-serif" }}
+															>
+																<h2 className="text-gray-900 title-font text-md font-bold font-sans truncate">
+																	{forum.forumName}
+																</h2>
+																{forum.isPrivate === 0 ? (
+																	<h2 className="text-blue-400 title-font text-md font-bold font-sans truncate">
+																		Public
+																	</h2>
+																) : (
+																	<h2 className="text-blue-400 title-font text-md font-bold font-sans truncate">
+																		Private
+																	</h2>
+																)}
+															</div>
+															<div className="my-4 bg-gray-200 h-[1px]"></div>
+															<h2
+																style={{ fontFamily: "Poppins, sans-serif" }}
+																className="text-gray-600 my-4 p-2 h-[1.8rem] title-font text-sm font-semibold text-center font-sans truncate"
+															>
+																{cityName}
+															</h2>
 														</div>
-														<div className="my-4 bg-gray-200 h-[1px]"></div>
-														<h2
-															style={{ fontFamily: "Poppins, sans-serif" }}
-															className="text-gray-600 my-4 p-2 h-[1.8rem] title-font text-sm font-semibold text-center font-sans truncate"
-														>
-															{cityName}
-														</h2>
 													</div>
-												</div>
-											))}
+												))}
+										</div>
 									</div>
-								</div>
-							) : (
-								<div>
-									<div className="flex items-center justify-center">
-										<h1
-											className=" m-auto mt-20 text-center font-sans font-bold text-2xl text-black"
+								) : (
+									<div>
+										<div className="flex items-center justify-center">
+											<h1
+												className=" m-auto mt-20 text-center font-sans font-bold text-2xl text-black"
+												style={{ fontFamily: "Poppins, sans-serif" }}
+											>
+												{t("currently_no_listings")}
+											</h1>
+										</div>
+										<div
+											className="m-auto mt-10 mb-40 text-center font-sans font-bold text-xl"
 											style={{ fontFamily: "Poppins, sans-serif" }}
 										>
-											{t("currently_no_listings")}
-										</h1>
+											<span className="font-sans text-black">
+												{t("to_upload_new_listing")}
+											</span>
+											<a
+												className="m-auto mt-20 text-center font-sans font-bold text-xl cursor-pointer text-blue-400"
+												style={{ fontFamily: "Poppins, sans-serif" }}
+												onClick={() => {
+													localStorage.setItem(
+														"selectedItem",
+														"Choose one category"
+													);
+												}}
+											>
+												{t("click_here")}
+											</a>
+										</div>
 									</div>
-									<div
-										className="m-auto mt-10 mb-40 text-center font-sans font-bold text-xl"
-										style={{ fontFamily: "Poppins, sans-serif" }}
-									>
-										<span className="font-sans text-black">
-											{t("to_upload_new_listing")}
-										</span>
-										<a
-											className="m-auto mt-20 text-center font-sans font-bold text-xl cursor-pointer text-blue-400"
-											style={{ fontFamily: "Poppins, sans-serif" }}
-											onClick={() => {
-												localStorage.setItem(
-													"selectedItem",
-													"Choose one category"
-												);
-											}}
-										>
-											{t("click_here")}
-										</a>
-									</div>
-								</div>
-							)}
-						</div>
+								)}
+							</div>
+						)}
 
 						<div className="mt-20 mb-20 w-fit mx-auto text-center text-white whitespace-nowrap rounded-md border border-transparent bg-blue-800 px-8 py-2 text-base font-semibold shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] cursor-pointer">
 							{pageNo !== 1 ? (
