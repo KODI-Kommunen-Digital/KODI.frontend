6b8c204b588e7aa222a6a799bad2d8193c238f5d
AkSu-HEIDI <akshay@heidi-app.de>
2023-09-20 11:38:55 +0200

    Basic version PDF upload and image upload merge and some bug fixes

--- src/Path/SubPages/EventDetails.js
+++ src/Path/SubPages/EventDetails.js
@@ -68,9 +68,10 @@ const EventDetails = () => {
 
 	useEffect(() => {
 		const searchParams = new URLSearchParams(window.location.search);
-		const pdfLoadingTimeout = setTimeout(() => {
-			setIsLoading(false);
-		}, 2000);
+		// const pdfLoadingTimeout = setTimeout(() => {
+		// 	setIsLoading(false);
+		// }, 2000);
+		setIsLoading(true);
 		const params = { statusId: 1 };
 		const cityId = searchParams.get("cityId");
 		setCityId(cityId);
@@ -124,7 +125,42 @@ const EventDetails = () => {
 					navigateTo("/Error");
 				});
 		}
-		return () => clearTimeout(pdfLoadingTimeout);
+		// return () => clearTimeout(pdfLoadingTimeout);
+		const fetchData = async () => {
+			try {
+				const response = await getListings(params);
+				const data = response.data.data;
+				console.log(response.data.data);
+				setListings(data);
+			} catch (error) {
+				console.error("Error fetching listings:", error);
+			} finally {
+				setIsLoading(false);
+			}
+		};
+		const fetchDataWithDelay = () => {
+			setTimeout(() => {
+				if (searchParams.has("categoryId")) {
+					const categoryId = searchParams.get("categoryId");
+					if (searchParams.has("sortByStartDate")) {
+						const params = { categoryId, sortByStartDate: true };
+						fetchData(params);
+					} else {
+						const params = { categoryId };
+						fetchData(params);
+					}
+				} else if (searchParams.has("cityId")) {
+					const cityId = searchParams.get("cityId");
+					const params = { cityId };
+					fetchData(params);
+				} else {
+					fetchData();
+				}
+			}, 2000);
+		};
+
+		fetchDataWithDelay();
+		return () => setIsLoading(false);
 	}, [t, cityId, window.location.href, isLoggedIn]);
 
 	const navigate = useNavigate();
@@ -172,7 +208,7 @@ const EventDetails = () => {
 	}, [user]);
 
 	const [handleClassName, setHandleClassName] = useState(
-		"rounded-md bg-white border border-gray-900 text-gray-900 py-2 px-4 text-sm cursor-pointer"
+		"rounded-xl bg-white border border-gray-900 text-gray-900 py-2 px-4 text-sm cursor-pointer"
 	);
 	const [, setFavButton] = useState("Favorite");
 	const handleFavorite = async (event) => {
