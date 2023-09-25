0161c71904ae04074c844d68d7475e03d65395ad
Moiz-HEIDI <130048960+Moiz-HEIDI@users.noreply.github.com>
2023-09-25 10:21:44 +0200

    catagories changed

--- src/Constants/categories.js
+++ src/Constants/categories.js
@@ -1,26 +1,26 @@
 export const categoryByName = {
-	news: 1,
-	events: 3,
-	clubs: 4,
-	regionalProducts: 5,
-	offerSearch: 6,
-	newCitizenInfo: 7,
-	lostAndFound: 9,
-	companyPortaits: 10,
-	carpoolingPublicTransport: 11,
-	offers: 12,
-	eatOrDrink: 13,
+  news: 1,
+  events: 3,
+  clubs: 4,
+  //   regionalProducts: 5,
+  offerSearch: 6,
+  //   newCitizenInfo: 7,
+  //   lostAndFound: 9,
+  companyPortaits: 10,
+  //   carpoolingPublicTransport: 11,
+  offers: 12,
+  eatOrDrink: 13,
 };
 export const categoryById = {
-	1: "news", // Nachricht
-	3: "events", // Veranstaltungen
-	4: "clubs", // Vereine
-	5: "regionalProducts", // Regionale Produkte
-	6: "offerSearch", // Ich biete / suche
-	7: "newCitizenInfo", // Neubürgerinfos
-	9: "lostAndFound", // Fundbüro
-	10: "companyPortaits", // Firmenportraits
-	11: "carpoolingPublicTransport", // Mitfahrbank / ÖPNV
-	12: "offers", // Angebote
-	13: "eatOrDrink", // Essen / Trinken
+  1: "news", // Nachricht
+  3: "events", // Veranstaltungen
+  4: "clubs", // Vereine
+  // 5: "regionalProducts", // Regionale Produkte
+  6: "offerSearch", // Ich biete / suche
+  // 7: "newCitizenInfo", // Neubürgerinfos
+  //   9: "lostAndFound", // Fundbüro
+  10: "companyPortaits", // Firmenportraits
+  //   11: "carpoolingPublicTransport", // Mitfahrbank / ÖPNV
+  12: "offers", // Angebote
+  13: "eatOrDrink", // Essen / Trinken
 };
