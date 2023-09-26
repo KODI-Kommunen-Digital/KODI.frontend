6bc0e43303580b55ae1922d66752ed3c53b6b083
AkSu-HEIDI <akshay@heidi-app.de>
2023-09-26 13:42:52 +0200

    Google and Apple store changes

--- src/Path/HomePage.jsx
+++ src/Path/HomePage.jsx
@@ -14,6 +14,8 @@ import ONEIMAGE from "../assets/01.png";
 import TWOIMAGE from "../assets/02.png";
 import THREEIMAGE from "../assets/03.png";
 import PdfToImage from "../Components/PdfToImage";
+import APPLESTORE from "../assets/apple-store-logo.png";
+import GOOGLEPLAYSTORE from "../assets/google-play-store-logo.png";
 
 const HomePage = () => {
 	const { t } = useTranslation();
@@ -134,7 +136,7 @@ const HomePage = () => {
 								className="object-cover object-center h-full w-full"
 								src={process.env.REACT_APP_BUCKET_HOST + "admin/Homepage.jpg"}
 							/>
-							<div className="absolute inset-0 flex flex-col items-center justify-center bg-gray-800 bg-opacity-50 text-white z--1">
+							<div className="absolute inset-0 flex flex-col gap-4 items-center justify-center bg-gray-800 bg-opacity-50 text-white z--1">
 								<h1
 									className="font-sans mb-8 lg:mb-12 text-4xl md:text-6xl lg:text-7xl text-center font-bold tracking-wide"
 									style={{
@@ -170,6 +172,27 @@ const HomePage = () => {
 											</option>
 										))}
 									</select>
+								</div>
+								<div className="flex flex-row mt-4 md:gap-8 gap-2 cursor-pointer">
+									<img
+										alt="ecommerce"
+										className="object-cover object-center md:h-8 h-8 w-50 m-auto"
+										src={APPLESTORE}
+										onClick={() => {
+											if (process.env.REACT_APP_REGION_NAME === "WALDI") {
+												window.location.href =
+													"https://apps.apple.com/de/app/waldi/id6463143260";
+											} else {
+												window.location.href =
+													"https://apps.apple.com/de/app/auf-app/id6463142195?l=en-GB";
+											}
+										}}
+									/>
+									<img
+										alt="ecommerce"
+										className="object-cover object-center md:h-8 h-8 w-50 m-auto"
+										src={GOOGLEPLAYSTORE}
+									/>
 								</div>
 							</div>
 						</div>
