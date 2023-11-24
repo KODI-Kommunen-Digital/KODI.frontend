e20c59f11dcce831dad9986c4483e8ffcf3aaea2
AkSu-HEID <akshay@heidi-app.de>
2023-11-24 10:55:28 +0100

    Removing comments

--- src/Path/UploadListings.jsx
+++ src/Path/UploadListings.jsx
@@ -392,22 +392,6 @@ function UploadListings() {
 				} else {
 					return "";
 				}
-
-			// case "endDate":
-			// 	if (parseInt(input.categoryId) === 3) {
-			// 		if (!value) {
-			// 			return t("pleaseEnterEndDate");
-			// 		} else {
-			// 			if (new Date(input.startDate) > new Date(value)) {
-			// 				return t("endDateBeforeStartDate");
-			// 			} else {
-			// 				return "";
-			// 			}
-			// 			return "";
-			// 		}
-			// 	} else {
-			// 		return "";
-			// 	}
 			default:
 				return "";
 		}
