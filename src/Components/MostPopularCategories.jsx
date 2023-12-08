8c2f17b73507052bdd20c070b33e73010ed366ee
AjUm-HEIDI <ajay@heidi-app.de>
2023-12-08 15:19:27 +0100

    Remove style tags

--- src/Components/MostPopularCategories.jsx
+++ src/Components/MostPopularCategories.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from 'prop-types';
 import { categoryIcons } from '../Constants/categoryIcons'
+import "../index.css";
 const MostPopularCategories = ({ listingsCount, t, goToAllListingsPage }) => {
 
     MostPopularCategories.propTypes = {
@@ -10,21 +11,10 @@ const MostPopularCategories = ({ listingsCount, t, goToAllListingsPage }) => {
     };
 
     return (
-        <div className="bg-white lg:px-10 md:px-5 sm:px-0 px-2 py-6 mt-10 mb-10 flex flex-col">
+        <div className="bg-white lg:px-10 md:px-5 sm:px-0 px-2 py-6 mt-10 mb-10 flex flex-col most-popular-category">
             <div className="grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-2 gap-4 relative mb-4 justify-center place-items-center">
-                <style>
-                    {`
-							@media (max-width: 280px) {
-							.grid-cols-2 {
-								grid-template-columns: repeat(1, minmax(0, 1fr));
-							}
-							}
-						`}
-                </style>
-
                 {listingsCount.map((listing) => {
                     const categoryIcon = categoryIcons[listing.categoryId] || { categoryName: "unknownCategory", className: "h-20 w-20 bg-cyan-400 flex items-center justify-center rounded-full m-auto shadow-xl" }
-
                     return (
                         <div
                             key={listing.categoryId}
