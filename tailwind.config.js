dc6e35d4f14a0d18c5a2b2dc2e5fba52b935bac6
AkSu-HEID <akshay@heidi-app.de>
2024-01-09 14:04:46 +0530

    error fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,43 +1,22 @@
 /** @type {import('tailwindcss').Config} */
-const withMT = require("@material-tailwind/react/utils/withMT");
-module.exports = withMT({
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+module.exports = {
+  content: [
+      "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {
-      scale: {
-        102: "1.02",
+      extend: {
+          scale: {
+              '102':'1.02'
+          },
+          margin: {
+              '5rem':'1rem'
+          }
       },
-      margin: {
-        "5rem": "1rem",
-      },
-    },
   },
   variants: {
-    extend: {
-      opacity: ["disabled"],
-    },
-    plugins: [],
+      extend: {
+          opacity: ['disabled']
+      }
+  },
+  plugins: [],
 }
-
-// THE BELOW CODE IS NOT USED BECAUSE OF BG WHITENING ISSUE
-// /** @type {import('tailwindcss').Config} */
-// const withMT = require("@material-tailwind/react/utils/withMT");
-// module.exports = withMT({
-//   content: ["./src/**/*.{js,jsx,ts,tsx}"],
-//   theme: {
-//     extend: {
-//       scale: {
-//         102: "1.02",
-//       },
-//       margin: {
-//         "5rem": "1rem",
-//       },
-//     },
-//   },
-//   variants: {
-//     extend: {
-//       opacity: ["disabled"],
-//     },
-//   },
-//   plugins: [require("@tailwindcss/aspect-ratio")],
-// });
