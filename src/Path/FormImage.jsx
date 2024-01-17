097fca6fd31eb54dadda78f17eba4601a7a63df3
AkSu-HEID <akshay@heidi-app.de>
2024-01-17 15:27:37 +0530

    bug and issue fix

--- src/Path/FormImage.jsx
+++ src/Path/FormImage.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import PropTypes from "prop-types";
-import { UploadSVG } from "../assets/icons/upload";
 
 function FormImage(props) {
   const { t } = useTranslation();
@@ -14,54 +13,35 @@ function FormImage(props) {
   };
 
   return Array.from(props.image).map((img, index) => {
-    const isDefaultImage =
-      typeof img === "string" && img.includes("Defaultimage1.png");
-
     const imageUrl =
       props.localImageOrPdf && img instanceof Blob
         ? URL.createObjectURL(img)
         : process.env.REACT_APP_BUCKET_HOST + img;
 
-        return (
-          isDefaultImage ? (
-            <div className="text-center" key={index}>
-              <UploadSVG />
-              <p className="mt-1 text-sm text-gray-600">
-                {t("dragAndDropImageOrPDF")}
-              </p>
-              <div className="relative mb-4 mt-8">
-                <label
-                  className={`file-upload-btn w-full bg-black hover:bg-slate-600 text-white font-bold py-2 px-4 rounded`}
-                >
-                  <span className="button-label">{t("upload")}</span>
-                  <input
-                    id="file-upload"
-                    type="file"
-                    accept="image/*,.pdf"
-                    className="sr-only"
-                    onChange={props.handleInputChange}
-                    multiple
-                  />
-                </label>
-              </div>
-            </div>
-          ) : (
-            <div className="flex flex-col items-center px-1" key={index}>
-              <img
-                className="object-cover h-64 w-full mb-4"
-                src={imageUrl}
-                alt="uploaded"
-              />
-              <button
-                className="w-full bg-black hover:bg-slate-600 text-white font-bold py-2 px-4 rounded"
-                onClick={() => updateListing(index)}
-              >
-                {t("removeFile")}
-              </button>
-            </div>
-          )
-        );
-        
+    // Check if the image is the default image
+    const isDefaultImage =
+      typeof img === "string" && img.includes("Defaultimage1.png");
+
+    // Render the image only if it's not the default image
+    if (!isDefaultImage) {
+      return (
+        <div className="flex flex-col items-center px-1" key={index}>
+          <img
+            className="object-cover h-64 w-full mb-4"
+            src={imageUrl}
+            alt="uploaded"
+          />
+          <button
+            className="w-full bg-black hover:bg-slate-600 text-white font-bold py-2 px-4 rounded"
+            onClick={() => updateListing(index)}
+          >
+            {t("removeFile")}
+          </button>
+        </div>
+      );
+    }
+
+    return null; // Don't render anything for the default image
   });
 }
 
