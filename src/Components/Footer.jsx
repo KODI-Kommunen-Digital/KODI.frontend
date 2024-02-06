247cdfa19ddf2d153a6297d59a30e24e068d7fa2
AkSu-HEID <akshay@heidi-app.de>
2024-02-06 10:51:21 +0100

    Improvements

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -149,21 +149,18 @@ function Footer(props) {
               </a>
             </p>
 
-            {(process.env.REACT_APP_REGION_NAME === "AUF" ||
-              process.env.REACT_APP_REGION_NAME === "GESEKE" ||
-              process.env.REACT_APP_REGION_NAME === "HEIDI") && (
-              <p className={`${footerClass} mb-4`}>
-                <a href="/TermsOfUse" className="text-gray-600 font-sans">
-                  {t("termsofuse")}
-                </a>
-              </p>
-            )}
-            {process.env.REACT_APP_REGION_NAME === "WALDI" && (
+            {process.env.REACT_APP_REGION_NAME === "WALDI" ? (
               <p className={`${footerClass} mb-4`}>
                 <a
                   href="https://ilzerland.bayern/nutzungsbedingungen-waldi/"
                   className="text-gray-600 font-sans"
                 >
+                  {t("termsofuse")}
+                </a>
+              </p>
+            ) : (
+              <p className={`${footerClass} mb-4`}>
+                <a href="/TermsOfUse" className="text-gray-600 font-sans">
                   {t("termsofuse")}
                 </a>
               </p>
