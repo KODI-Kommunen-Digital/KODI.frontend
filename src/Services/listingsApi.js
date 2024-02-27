371811efc90ea86d4e5f87beec03bde44b41f299
SoSa-HEIDI <sonu@heidi-app.de>
2024-02-27 11:08:14 +0100

    changed showExternal to true

--- src/Services/listingsApi.js
+++ src/Services/listingsApi.js
@@ -7,7 +7,7 @@ export async function getAllListings() {
 }
 
 export async function getListings(params) {
-	params.showExternalListings = "false";
+	params.showExternalListings = "true";
 	return axios.get(`/listings`, { params });
 }
 
