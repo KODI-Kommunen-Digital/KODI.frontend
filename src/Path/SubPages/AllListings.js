6a3c5fc3bfba3dc4603217cf818b48f2adf8224c
SoSa-HEIDI <sonu@heidi-app.de>
2024-02-28 14:29:48 +0100

    changed the official notification button function name

--- src/Path/SubPages/AllListings.js
+++ src/Path/SubPages/AllListings.js
@@ -169,7 +169,7 @@ const AllListings = () => {
     }
   };
 
-  const handleAmtlicheMitteilungButton = () => {
+  const handleOfficialNotificationButton = () => {
     setCategoryId(16)
     navigateTo("/AllListings?terminalView=true&categoryId=16")
   };
@@ -323,7 +323,7 @@ const AllListings = () => {
       <div className="mt-5 mb-20 customproview py-6">
         {terminalViewParam && (<div className="text-center mt-4 mb-4">
           <button
-            onClick={handleAmtlicheMitteilungButton}
+            onClick={handleOfficialNotificationButton}
             className="text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Amtliche Mitteilung
