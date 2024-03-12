5a0baa59249c25edcc993100293dcf810f3ae69c
AkSu-HEID <akshay@heidi-app.de>
2024-03-12 14:24:50 +0100

    Waldi changes

--- src/Path/CitizenService.jsx
+++ src/Path/CitizenService.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import Footer from "../Components/Footer";
 import { getCities, getCitizenServices } from "../Services/cities";
+import LoadingPage from "../Components/LoadingPage";
 
 const CitizenService = () => {
   window.scrollTo(0, 0);
@@ -12,6 +13,7 @@ const CitizenService = () => {
   const [citizenService, setCitizenServices] = useState({});
   const [citiesArray, setCitiesArray] = useState([]);
   const [cityId, setCityId] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
   const navigate = useNavigate();
   const navigateTo = (path) => {
     if (path) {
@@ -21,27 +23,37 @@ const CitizenService = () => {
   };
 
   useEffect(() => {
-    const urlParams = new URLSearchParams(window.location.search);
-    document.title =
-      process.env.REACT_APP_REGION_NAME + " " + t("citizenService");
-    getCities().then((response) => {
-      setCitiesArray(response.data.data);
-      const temp = {};
-      for (const city of response.data.data) {
-        temp[city.id] = {
-          name: city.name,
-          hasForum: city.hasForum,
-        };
-      }
-      setCities(temp);
-      const cityIdParam = urlParams.get("cityId");
-      if (cityIdParam && temp[cityIdParam]) setCityId(cityIdParam);
-    });
-
-    getCitizenServices().then((response) => {
-      setCitizenServices(response.data.data);
-    });
+    const timeout = setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+    return () => clearTimeout(timeout);
   }, []);
+
+  useEffect(() => {
+    if (!isLoading) {
+      const urlParams = new URLSearchParams(window.location.search);
+      document.title =
+        process.env.REACT_APP_REGION_NAME + " " + t("citizenService");
+      getCities().then((response) => {
+        setCitiesArray(response.data.data);
+        const temp = {};
+        for (const city of response.data.data) {
+          temp[city.id] = {
+            name: city.name,
+            hasForum: city.hasForum,
+          };
+        }
+        setCities(temp);
+        const cityIdParam = urlParams.get("cityId");
+        if (cityIdParam && temp[cityIdParam]) setCityId(cityIdParam);
+      });
+
+      getCitizenServices().then((response) => {
+        setCitizenServices(response.data.data);
+      });
+    }
+  }, [isLoading, t]);
+
 
   const handleLinkClick = (data) => {
     if (data.isExternalLink) {
@@ -60,15 +72,6 @@ const CitizenService = () => {
     }
   };
 
-  // const handleLinkClick = (data) => {
-  // 	if (data.isExternalLink) {
-  // 		window.open(data.link, "_blank");
-  // 	} else {
-  // 		const urlWithCityId = cityId ? `${data.link}?cityId=${cityId}` : data.link;
-  // 		navigateTo(urlWithCityId);
-  // 	}
-  // };
-
   return (
     <section className="text-gray-600 bg-white body-font">
       <HomePageNavBar />
@@ -84,7 +87,11 @@ const CitizenService = () => {
               />
               <div className="absolute inset-0 flex flex-col items-center justify-center bg-gray-800 bg-opacity-50 text-white z--1">
                 <h1 className="text-4xl md:text-6xl lg:text-7xl text-center font-bold mb-4 font-sans">
-                  {t("citizenService")}
+                  {process.env.REACT_APP_REGION_NAME === "WALDI" ? (
+                    t("moreService")
+                  ) : (
+                    t("citizenService")
+                  )}
                 </h1>
 
                 <div className="col-span-6 sm:col-span-1 mt-1 w-auto px-0 mr-0">
@@ -128,52 +135,57 @@ const CitizenService = () => {
         </div>
       </div>
 
-      {citizenService && citizenService.length > 0 ? (
-        <div className="bg-white lg:px-10 md:px-5 sm:px-0 px-2 py-6 mt-10 mb-10 space-y-10 flex flex-col">
-          <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4 relative mb-4 justify-center place-items-center">
-            {citizenService &&
-              citizenService
-                // .filter((data) => data.title !== "forums" || showForum)
-                .map((data, index) => (
-                  <div
-                    key={index}
-                    className="h-80 w-full rounded-lg cursor-pointer transition-all duration-300 hover:shadow-xl transform hover:-translate-y-2"
-                  >
-                    <div className="relative h-80 rounded overflow-hidden">
-                      <a
-                        rel="noreferrer noopener"
-                        onClick={() => handleLinkClick(data)}
-                      >
-                        <img
-                          alt={data.title}
-                          className="object-cover object-center h-full w-full"
-                          src={process.env.REACT_APP_BUCKET_HOST + data.image}
-                        />
-                        <div className="absolute inset-0 flex flex-col justify-end bg-opacity-50 text-white z--1">
-                          <p className="mb-4 ml-4 font-sans">
-                            {cities[data.cityId]}
-                          </p>
-                        </div>
-                      </a>
-                    </div>
-                  </div>
-                ))}
-          </div>
-        </div>
+      {isLoading ? (
+        <LoadingPage />
       ) : (
-        <div className="md:mb-60 mt-20 mb-20 p-6">
-          <div className="text-center">
-            <div className="m-auto md:mt-20 mt-0 mb-20 text-center font-sans font-bold text-xl">
-              <h1 className="text-5xl md:text-8xl lg:text-10xl text-center font-bold my-10 font-sans bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 text-transparent bg-clip-text">
-                Oops !
-              </h1>
+        <div>
+          {citizenService && citizenService.length > 0 ? (
+            <div className="bg-white lg:px-10 md:px-5 sm:px-0 px-2 py-6 mt-10 mb-10 space-y-10 flex flex-col">
+              <div className="grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4 relative mb-4 justify-center place-items-center">
+                {citizenService &&
+                  citizenService
+                    .map((data, index) => (
+                      <div
+                        key={index}
+                        className="h-80 w-full rounded-lg cursor-pointer transition-all duration-300 hover:shadow-xl transform hover:-translate-y-2"
+                      >
+                        <div className="relative h-80 rounded overflow-hidden">
+                          <a
+                            rel="noreferrer noopener"
+                            onClick={() => handleLinkClick(data)}
+                          >
+                            <img
+                              alt={data.title}
+                              className="object-cover object-center h-full w-full"
+                              src={process.env.REACT_APP_BUCKET_HOST + data.image}
+                            />
+                            <div className="absolute inset-0 flex flex-col justify-end bg-opacity-50 text-white z--1">
+                              <p className="mb-4 ml-4 font-sans">
+                                {cities[data.cityId]}
+                              </p>
+                            </div>
+                          </a>
+                        </div>
+                      </div>
+                    ))}
+              </div>
             </div>
-            <div className="m-auto mt-20 mb-20 text-center font-sans font-bold text-xl">
-              <h1 className=" m-auto mt-20 text-center font-sans font-bold text-2xl text-black">
-                {t("currently_no_services")}
-              </h1>
+          ) : (
+            <div className="md:mb-60 mt-20 mb-20 p-6">
+              <div className="text-center">
+                <div className="m-auto md:mt-20 mt-0 mb-20 text-center font-sans font-bold text-xl">
+                  <h1 className="text-5xl md:text-8xl lg:text-10xl text-center font-bold my-10 font-sans bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 text-transparent bg-clip-text">
+                    Oops !
+                  </h1>
+                </div>
+                <div className="m-auto mt-20 mb-20 text-center font-sans font-bold text-xl">
+                  <h1 className=" m-auto mt-20 text-center font-sans font-bold text-2xl text-black">
+                    {t("currently_no_services")}
+                  </h1>
+                </div>
+              </div>
             </div>
-          </div>
+          )}
         </div>
       )}
 
--- src/Path/HomePage.jsx
+++ src/Path/HomePage.jsx
@@ -491,41 +491,43 @@ const HomePage = () => {
         </div>
       </div>
 
-      <div className="bg-blue-400 mx-auto py-10 px-4 flex justify-center lg:h-[28rem] sm:h-[35rem]">
-        <div className="flex flex-wrap items-center">
-          <div className="w-full md:w-1/2 px-4">
-            <h2
-              className="text-4xl text-white font-bold mb-4 font-sans"
-              style={{ fontFamily: "Poppins, sans-serif" }}
-            >
-              {t("citizenService")}
-            </h2>
-            <p
-              className="mb-4 text-gray-900 text-lg font-bold font-sans"
-              style={{ fontFamily: "Poppins, sans-serif" }}
-            >
-              {t("findBestCitizenServicesInTheCity")}
-            </p>
-            <a
-              onClick={() => goToCitizensPage()}
-              className="ml-0 w-full sm:w-48 font-sans inline-flex items-center justify-center whitespace-nowrap rounded-xl border border-transparent bg-blue-800 px-8 py-2 text-base font-semibold text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] cursor-pointer"
-              style={{ fontFamily: "Poppins, sans-serif" }}
-            >
-              {t("clickHereToFind")}
-            </a>
-          </div>
+      {process.env.REACT_APP_REGION_NAME !== "WALDI" && (
+        <div className="bg-blue-400 mx-auto py-10 px-4 flex justify-center lg:h-[28rem] sm:h-[35rem]">
+          <div className="flex flex-wrap items-center">
+            <div className="w-full md:w-1/2 px-4">
+              <h2
+                className="text-4xl text-white font-bold mb-4 font-sans"
+                style={{ fontFamily: "Poppins, sans-serif" }}
+              >
+                {t("citizenService")}
+              </h2>
+              <p
+                className="mb-4 text-gray-900 text-lg font-bold font-sans"
+                style={{ fontFamily: "Poppins, sans-serif" }}
+              >
+                {t("findBestCitizenServicesInTheCity")}
+              </p>
+              <a
+                onClick={() => goToCitizensPage()}
+                className="ml-0 w-full sm:w-48 font-sans inline-flex items-center justify-center whitespace-nowrap rounded-xl border border-transparent bg-blue-800 px-8 py-2 text-base font-semibold text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] cursor-pointer"
+                style={{ fontFamily: "Poppins, sans-serif" }}
+              >
+                {t("clickHereToFind")}
+              </a>
+            </div>
 
-          <div className="w-full md:w-1/2 flex flex-wrap lg:mt-0 md:mt-6 mt-6">
-            <img
-              src={
-                process.env.REACT_APP_BUCKET_HOST + "admin/CitizenService2.png"
-              }
-              alt="Image 1"
-              className="w-full md:w-98 mb-2"
-            />
+            <div className="w-full md:w-1/2 flex flex-wrap lg:mt-0 md:mt-6 mt-6">
+              <img
+                src={
+                  process.env.REACT_APP_BUCKET_HOST + "admin/CitizenService2.png"
+                }
+                alt="Image 1"
+                className="w-full md:w-98 mb-2"
+              />
+            </div>
           </div>
         </div>
-      </div>
+      )}
 
       <div className="bottom-0 w-full">
         <Footer />
