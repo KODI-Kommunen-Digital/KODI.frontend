c2acb583c2c93f5bc88ee4670a9d3a020a844c4a
AjUm-HEIDI <ajay@heidi-app.de>
2024-04-02 16:02:19 +0530

    Set correct user role

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -165,7 +165,7 @@ function SideBar() {
 							{t("uploadPost")}
 						</span>
 					</div>
-					{userRole === role.User && (
+					{userRole === role.Admin && (
 						<div
 							className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-slate-600 text-white"
 							onClick={() => {
