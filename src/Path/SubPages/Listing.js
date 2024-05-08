bc37f611c14487ad157d6bb27df5e498c9886a00
AkSu-HEID <akshay@heidi-app.de>
2024-05-08 16:09:22 +0200

    Merge fix

--- src/Path/SubPages/Listing.js
+++ src/Path/SubPages/Listing.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import PROFILEIMAGE from "../../assets/ProfilePicture.png";
 import HomePageNavBar from "../../Components/HomePageNavBar";
-import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import { getListings, getListingsById } from "../../Services/listingsApi";
 import { getProfile } from "../../Services/usersApi";
@@ -25,6 +24,7 @@ import { getCategory } from "../../Services/CategoryApi";
 import PDFDisplay from "../../Components/PdfViewer";
 import { listingSource } from "../../Constants/listingSource";
 import RegionColors from "../../Components/RegionColors";
+import { useNavigate, useLocation } from "react-router-dom";
 
 const Description = (props) => {
   const [desc, setDesc] = useState();
@@ -147,6 +147,9 @@ const Listing = () => {
   useEffect(() => {
     document.title =
       process.env.REACT_APP_REGION_NAME + " " + t("eventDetails");
+    const searchParams = new URLSearchParams(location.search);
+    const terminalViewParam = searchParams.get("terminalView");
+    setTerminalView(terminalViewParam === "true");
     getCategory().then((response) => {
       const catList = {};
       response?.data.data.forEach((cat) => {
@@ -624,6 +627,22 @@ const Listing = () => {
                       </div>
                     </div>
                   </div>
+                )}
+
+                {input.id && input.categoryId === 18 && (
+                  <a
+                    onClick={() =>
+                      navigateTo(`/Listings/BookAppointments?listingId=${listingId}&cityId=${cityId}&appointmentId=${appointmentId}`)
+                    }
+                    className="relative w-full mt-4 inline-flex items-center justify-center p-4 px-6 py-3 overflow-hidden font-medium text-black transition duration-300 ease-out border-2 border-black rounded-full shadow-md group cursor-pointer">
+                    <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-black group-hover:translate-x-0 ease">
+                      <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3"></path></svg>
+                    </span>
+                    <span className="absolute flex items-center justify-center w-full h-full text-black transition-all duration-300 transform group-hover:translate-x-full ease">{t("clickHereToBook")}</span>
+                    <span className="relative invisible">
+                      {t("clickHereToBook")}
+                    </span>
+                  </a>
                 )}
               </div>
             </div>
