d071c054c732a790d7f79603907a820520627242
AkSu-HEID <akshay@heidi-app.de>
2024-06-06 14:59:31 +0200

    UI issue fix

--- src/Components/LoadingPage.jsx
+++ src/Components/LoadingPage.jsx
@@ -11,11 +11,12 @@ const LoadingPage = () => {
     <div className={location.pathname === '/' ? "relative h-full flex items-center justify-center bg-white" : "fixed inset-0 flex items-center justify-center bg-white"}>
 
       <div className="text-center">
-        <h1 className={`text-2xl md:text-4xl lg:text-6xl text-center font-bold py-20 font-sans bg-clip-text text-transparent ${colors.darkTextColor}`}>
+        <h1 className={`text-2xl md:text-4xl lg:text-6xl text-center font-bold py-20 font-sans bg-clip-text ${colors.darkTextColor}`}>
           {t("loading")}
         </h1>
         <div className="my-10 flex">
-          <div className={`relative mx-auto animate-[rotate_1s_infinite] loading-wheel border-8  ${colors.lightBorderColor}`}></div>
+          <div className={`relative mx-auto h-24 w-24 rounded-full border-t-8 border-b-8  ${colors.lightBorderColor} animate-spin`}></div>
+          {/* <div className={`relative mx-auto animate-[rotate_1s_infinite] loading-wheel border-8  ${colors.lightBorderColor}`}></div> */}
         </div>
         <p className={`text-lg py-20 md:text-xl lg:text-4xl font-bold ${colors.lightTextColor}`}>
           {t("youAreAlmostThere")}
