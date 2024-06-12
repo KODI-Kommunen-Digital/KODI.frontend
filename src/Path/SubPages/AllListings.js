1bd7e1a5a51a631aa7140cbcb756cc5048e8e44e
AjUm-HEIDI <ajay@heidi-app.de>
2024-06-12 09:19:15 +0200

    Update category order in all listings

--- src/Path/SubPages/AllListings.js
+++ src/Path/SubPages/AllListings.js
@@ -12,7 +12,7 @@ import {
 import { useTranslation } from "react-i18next";
 import { getListings, getListingsBySearch } from "../../Services/listingsApi";
 import { getCities } from "../../Services/cities";
-// import { categoryByName, categoryById } from "../../Constants/categories";
+import { categoryById } from "../../Constants/categories";
 import Footer from "../../Components/Footer";
 import LoadingPage from "../../Components/LoadingPage";
 import { getCategory } from "../../Services/CategoryApi";
@@ -52,11 +52,7 @@ const AllListings = () => {
     setIsLoading(true);
     Promise.all([getCities(), getCategory()]).then((response) => {
       setCities(response[0].data.data);
-      const catList = {};
-      response[1]?.data.data.forEach((cat) => {
-        catList[cat.id] = cat.name;
-      });
-      setCategories(catList);
+      setCategories(response[1]?.data?.data || []);
       const params = { pageSize, statusId: 1 };
       const pageNoParam = parseInt(urlParams.get("pageNo"));
       if (pageNoParam > 1) {
@@ -81,9 +77,9 @@ const AllListings = () => {
       const categoryIdParam = urlParams.get("categoryId");
       if (categoryIdParam) {
         const categoryId = parseInt(categoryIdParam);
-        if (catList[categoryId]) {
+        if (categoryById[categoryId]) {
           setCategoryId(categoryId);
-          setCategoryName(t(catList[categoryId]));
+          setCategoryName(t(categoryById[categoryId]));
           params.categoryId = categoryId;
           if (categoryId === 3) {
             params.sortByStartDate = true;
@@ -114,7 +110,7 @@ const AllListings = () => {
         urlParams.delete("cityId");
       }
       if (parseInt(categoryId)) {
-        setCategoryName(t(categories[categoryId]));
+        setCategoryName(t(categoryById[categoryId]));
         params.categoryId = parseInt(categoryId);
         urlParams.set("categoryId", parseInt(categoryId));
       } else {
@@ -303,7 +299,7 @@ const AllListings = () => {
                       name="category"
                       autoComplete="category-name"
                       onChange={(e) => setCategoryId(e.target.value)}
-                      value={categoryId}
+                      value={categoryId || 0}
                       className="bg-white h-10 px-5 pr-10 rounded-full text-sm focus:outline-none w-full text-gray-600"
                       style={{
                         fontFamily: "Poppins, sans-serif",
@@ -312,10 +308,10 @@ const AllListings = () => {
                       <option className="font-sans" value={0} key={0}>
                         {t("allCategories")}
                       </option>
-                      {Object.keys(categories).map((key) => {
+                      {categories.map((category) => {
                         return (
-                          <option className="font-sans" value={key} key={key}>
-                            {t(categories[key])}
+                          <option className="font-sans" value={category.id} key={category.id}>
+                            {t(category.name)}
                           </option>
                         );
                       })}
