4f66877f8ef242cbc051217023742578eee238f4
AkSu-HEID <akshay@heidi-app.de>
2024-06-12 15:02:48 +0200

    UI changes

--- src/Path/AllDevices.jsx
+++ src/Path/AllDevices.jsx
@@ -185,13 +185,13 @@ const AllDevices = () => {
 							{t("logoutOfAllDevices")}
 						</button>
 						{showConfirmationModal.visible && (
-							<div className="fixed z-10 inset-0 overflow-y-auto">
-								<div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+							<div className="fixed z-50 inset-0 flex items-center justify-center overflow-y-auto">
+								<div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
 									<div
 										className="fixed inset-0 transition-opacity"
 										aria-hidden="true"
 									>
-										<div className="absolute inset-0 bg-gray-500 opacity-75"></div>
+										<div className="absolute inset-0 bg-gray-500 opacity-100"></div>
 									</div>
 									<span
 										className="hidden sm:inline-block sm:align-middle sm:h-screen"
