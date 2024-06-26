df964009f5c2d6d8cec153d76f7f404dea6a74ad
AkSu-HEID <akshay@heidi-app.de>
2024-06-26 12:01:24 +0200

    Bug fixes

--- src/Path/UploadListings.jsx
+++ src/Path/UploadListings.jsx
@@ -341,7 +341,7 @@ function UploadListings() {
         // Post or update listing data
         const response = await (newListing
           ? postListingsData(dataToSubmit)
-          : updateListingsData(dataToSubmit, listingId));
+          : updateListingsData(cityIds, dataToSubmit, listingId));
 
         let currentListingId = [];
         if (response && response.data && response.data.data && Array.isArray(response.data.data) && response.data.data.length > 0) {
@@ -522,7 +522,7 @@ function UploadListings() {
       // getVillages(cityId).then((response) => setVillages(response.data.data));
       getListingsById(cityIds, listingId).then((listingsResponse) => {
         const listingData = listingsResponse.data.data;
-        listingData.cityId = cityIds;
+        listingData.cityIds = cityIds;
         setListingInput(listingData);
         setDescription(listingData.description);
         setCategoryId(listingData.categoryId);
@@ -968,7 +968,7 @@ function UploadListings() {
             </div>
           </div>
 
-          {process.env.REACT_APP_MULTIPLECITYSELECTION === 'True' ? (
+          {process.env.REACT_APP_MULTIPLECITYSELECTION === 'True' && newListing ? (
             cities.length > 1 && (
               <div className="relative mb-4">
                 <label
@@ -980,7 +980,7 @@ function UploadListings() {
                 <select
                   id="cityIds"
                   name="cityIds"
-                  value=""
+                  value={cityIds || 0}
                   onChange={onCityChange}
                   disabled={!newListing}
                   className="overflow-y-scroll w-full bg-white rounded border border-gray-300 focus:border-black focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out shadow-md disabled:bg-gray-400"
