2a2036d61ed76fea92d29b2832b4c0c4b78692c1
Moiz-HEIDI <130048960+Moiz-HEIDI@users.noreply.github.com>
2024-09-04 14:06:32 +0200

    chaged for production

--- deployProd.sh
+++ deployProd.sh
@@ -3,7 +3,17 @@
 source .env
 react-scripts build
 if [[-n $PM2_PROCESS_NAME]]; then
-    pm2 serve build $PORT --spa --name $PM2_PROCESS_NAME
+    pm2 delete $PM2_PROCESS_NAME
+    if [[-n $PORT]]; then
+        pm2 serve build $PORT --spa --name $PM2_PROCESS_NAME
+    else
+        pm2 serve build 3000 --spa --name $PM2_PROCESS_NAME
+    fi
 else
-    pm2 serve build $PORT --spa --name "WEB"
+    pm2 delete "WEB"
+    if [[-n $PORT]]; then
+        pm2 serve build $PORT --spa --name "WEB"
+    else
+        pm2 serve build 3000 --spa --name "WEB"
+    fi
 fi
