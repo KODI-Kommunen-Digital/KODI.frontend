7511a9d97df087a9ab789e119941defcc14ea620
AkSu-HEID <akshay@heidi-app.de>
2024-09-16 14:17:33 +0200

    Motomo changes

--- src/index.js
+++ src/index.js
@@ -6,15 +6,27 @@ import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import { AuthProvider } from "./context/AuthProvider";
 import { ChakraProvider } from "@chakra-ui/react";
+import { MatomoProvider, createInstance } from '@datapunt/matomo-tracker-react';
+
+// Create the Matomo instance with your Matomo Cloud information
+const instance = createInstance({
+  urlBase: process.env.REACT_APP_MATOMO_URL_BASE,  // Matomo Cloud URL from env
+  siteId: process.env.REACT_APP_MATOMO_SITE_ID,  // Site ID from env
+  trackerUrl: process.env.REACT_APP_MATOMO_TRACKER_URL,  // Tracker URL from env
+  srcUrl: process.env.REACT_APP_MATOMO_SRC_URL,  // JavaScript URL from env
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AuthProvider>
-      <ChakraProvider>
-        <App />
-      </ChakraProvider>
-    </AuthProvider>
+    <MatomoProvider value={instance}>
+      <AuthProvider>
+        <ChakraProvider>
+          <App />
+        </ChakraProvider>
+      </AuthProvider>
+    </MatomoProvider>
   </React.StrictMode>
 );
+
 reportWebVitals();
