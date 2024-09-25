49089106e42d4ca627bfc4bd3cf6c245e20d34c6
AjUm-HEIDI <ajay@heidi-app.de>
2024-09-25 15:38:52 +0200

    Set up motamo conditionally

--- src/index.js
+++ src/index.js
@@ -8,24 +8,38 @@ import { AuthProvider } from "./context/AuthProvider";
 import { ChakraProvider } from "@chakra-ui/react";
 import { MatomoProvider, createInstance } from '@datapunt/matomo-tracker-react';
 
-// Create the Matomo instance with your Matomo Cloud information
-const instance = createInstance({
-  urlBase: process.env.REACT_APP_MATOMO_URL_BASE,  // Matomo Cloud URL from env
-  siteId: process.env.REACT_APP_MATOMO_SITE_ID,  // Site ID from env
-  trackerUrl: process.env.REACT_APP_MATOMO_TRACKER_URL,  // Tracker URL from env
-  srcUrl: process.env.REACT_APP_MATOMO_SRC_URL,  // JavaScript URL from env
-});
+// Check the Matomo status
+const matomoStatus = process.env.REACT_APP_MATOMO_STATUS === 'True';
+
+let instance;
+if (matomoStatus) {
+  // Create the Matomo instance only if the status is True
+  instance = createInstance({
+    urlBase: process.env.REACT_APP_MATOMO_URL_BASE,  // Matomo Cloud URL from env
+    siteId: process.env.REACT_APP_MATOMO_SITE_ID,    // Site ID from env
+    trackerUrl: process.env.REACT_APP_MATOMO_TRACKER_URL,  // Tracker URL from env
+    srcUrl: process.env.REACT_APP_MATOMO_SRC_URL,    // JavaScript URL from env
+  });
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <MatomoProvider value={instance}>
+    {matomoStatus ? (
+      <MatomoProvider value={instance}>
+        <AuthProvider>
+          <ChakraProvider>
+            <App />
+          </ChakraProvider>
+        </AuthProvider>
+      </MatomoProvider>
+    ) : (
       <AuthProvider>
         <ChakraProvider>
           <App />
         </ChakraProvider>
       </AuthProvider>
-    </MatomoProvider>
+    )}
   </React.StrictMode>
 );
 
