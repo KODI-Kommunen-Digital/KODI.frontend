a4c52e22b57881868f6359fe60b7f6b0a61ad4ba
AkSu-HEID <akshay@heidi-app.de>
2025-01-29 16:57:38 +0100

    UI fixes

--- src/Path/SubPages/Listing.jsx
+++ src/Path/SubPages/Listing.jsx
@@ -478,24 +478,21 @@ const Listing = () => {
                             </p>
                           </div>
 
-                          <div
-                            className={`hidden md:block flex items-center mt-6 ${terminalView ? "hidden" : "visible"
-                              }`}
-                          >
-                            <a
-                              onClick={() => handleFavorite()}
-                              className={`relative inline-flex items-center justify-start px-4 py-2 overflow-hidden font-medium transition-all bg-white rounded hover:bg-white group border cursor-pointer ${RegionColors.lightBorderColor}`}
-                            >
-                              <span
-                                className={`w-48 h-48 rounded rotate-[-40deg] ${RegionColors.lightBgColor} absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0`}
-                              ></span>
-                              <span className="relative w-full text-left text-slate-800 transition-colors duration-300 ease-in-out group-hover:text-white">
-                                {favoriteId !== 0
-                                  ? t("unfavorite")
-                                  : t("favourites")}
-                              </span>
-                            </a>
-                          </div>
+                          {!terminalView && (
+                            <div className="hidden md:block flex items-center mt-6">
+                              <a
+                                onClick={() => handleFavorite()}
+                                className={`relative inline-flex items-center justify-start px-4 py-2 overflow-hidden font-medium transition-all bg-white rounded hover:bg-white group border cursor-pointer ${RegionColors.lightBorderColor}`}
+                              >
+                                <span
+                                  className={`w-48 h-48 rounded rotate-[-40deg] ${RegionColors.lightBgColor} absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0`}
+                                ></span>
+                                <span className="relative w-full text-left text-slate-800 transition-colors duration-300 ease-in-out group-hover:text-white">
+                                  {favoriteId !== 0 ? t("unfavorite") : t("favourites")}
+                                </span>
+                              </a>
+                            </div>
+                          )}
                           <div
                             className={`md:hidden block flex items-center mt-6 ${terminalView ? "hidden" : "visible"
                               }`}
