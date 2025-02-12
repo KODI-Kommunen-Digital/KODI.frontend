63ead713a6f5be303888f432789e346c7e23dc66
AkSu-HEID <akshay@heidi-app.de>
2025-02-12 15:55:10 +0100

    Stockheim fixes

--- src/Path/V2/HomePage.jsx
+++ src/Path/V2/HomePage.jsx
@@ -399,7 +399,7 @@ const HomePage = () => {
 
               <div className="bg-white lg:px-20 md:px-5 px-5 py-6 mt-0 mb-10 space-y-10 flex flex-col">
                 <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4 relative mb-4 justify-center place-items-center">
-                  {cities.map((city) => {
+                  {(cities ?? []).map((city) => { // This ensures that cities is always an array, preventing errors like "Cannot read properties of undefined (reading 'map')"
                     if (city.id !== Number(cityId)) {
                       return (
                         <div
