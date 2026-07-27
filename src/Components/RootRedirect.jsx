a82d13c547f2557ce881a90e8a2e296657f60fd1
sameer-heidi <dev8@heidi-app.com>
2026-07-27 17:44:15 +0530

    feat: enhance RootRedirect to conditionally render HomePage based on terminalView and frontendVersion

--- src/Components/RootRedirect.jsx
+++ src/Components/RootRedirect.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { getCookie } from '../cookies/cookieServices';
+import HomePageV1 from '../Path/V1/HomePage';
+import HomePageV2 from '../Path/V2/HomePage';
 
 const RootRedirect = () => {
+  const searchParams = new URLSearchParams(window.location.search);
+  const isTerminalView = searchParams.get('terminalView') === 'true';
+  const frontendVersion = process.env.REACT_APP_FORNTENDVERSION || '1';
+
+  if (isTerminalView) {
+    return frontendVersion === '1' ? <HomePageV1 /> : <HomePageV2 />;
+  }
+
   const accessToken =
     window.localStorage.getItem('accessToken') ||
     window.sessionStorage.getItem('accessToken') ||
